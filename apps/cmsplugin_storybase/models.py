8e16f03cb3d6c81b7e8c9204a9e32577a27dfe8a
Geoffrey Hing <geoff@terrorware.com>
2012-02-08 02:11:58 -0600

    Fixed ForeignKey reference.

--- apps/cmsplugin_storybase/models.py
+++ apps/cmsplugin_storybase/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from cms.models.pluginmodel import CMSPlugin
-from storybase_story.models import Story
 
 class StoryPlugin(CMSPlugin):
-    story = models.ForeignKey(Story, related_name='plugins')
+    story = models.ForeignKey('storybase_story.Story', related_name='plugins')
 
     def __unicode__(self):
         return self.story.title
