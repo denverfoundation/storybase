aa1fc343113fa1d127f2d06efc2492067f40d2c4
Geoffrey Hing <geoff@terrorware.com>
2012-02-13 19:49:54 -0600

    Added really basic view for displaying a story and its assets.

--- apps/storybase_story/views.py
+++ apps/storybase_story/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.views.generic import DetailView
+from models import Story
+
+class StoryDetailView(DetailView):
+    context_object_name = "story"
+    model = Story
+
+    """
+    def get_context_data(self, **kwargs):
+        context = super(StoryDetailView, self).get_context_data(**kwargs)
+        return context
+    """
