610fa8cc3bb09c914b3a7e8b6594f538f1855989
Geoffrey Hing <geoff@terrorware.com>
2012-02-29 20:43:34 -0600

    Added configuration for building with Travis CI.

---
+++ settings/travis.py
@@ -0,0 +1,13 @@
+from defaults import *
+
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'atlas_test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
