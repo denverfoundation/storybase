3a448478a2e38d6c9ebb974c08dbaa882122f276
Geoffrey Hing <geoff@terrorware.com>
2012-02-29 22:18:27 -0600

    Have Django create the test database and not the Travis build script.

--- settings/travis.py
+++ settings/travis.py
@@ -11,3 +11,9 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+# These settings are required the create a spatial database when 
+# testing GeoDjango apps.
+# See http://geodjango.org/docs/testing.html?highlight=testing#testing-geodjango-apps
+TEST_RUNNER='django.contrib.gis.tests.run_tests'
+POSTGIS_TEMPLATE='template_postgis'
