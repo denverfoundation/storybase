24fd9a12e8ef0c73a4184bfee6a7d60906c75606
Geoffrey Hing <geoff@terrorware.com>
2012-03-07 17:25:32 -0600

    Added fields to Django Admin to be able to edit a User's Organization and Project membership in the User admin.
    The ManyToMany relationship between Organizations/Projects and Users
    is defined in those models (and not the User model).  This makes sense as
    the User model is defined by Django.  By default, the ManyToMany field
    is displayed in the model admin for the model where it is defined.  I
    had to do a little bit of hackery to allow for editing the relations
    through the User admin as well.
    
    http://blog.abiss.gr/mgogoulos/entry/many_to_many_relationships_and
    was very helpful in guiding me how to do this.

--- apps/storybase_user/admin.py
+++ apps/storybase_user/admin.py
@@ -1,10 +1,63 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext, ugettext_lazy as _
 from models import Organization, Project
 
+class StoryUserAdminForm(UserChangeForm):
+    """ 
+    Custom form for editing users in the Django admin
+
+    This adds fields for the ManyToMany relations to Organization and 
+    Project models.  These aren't included automatically because they're
+    the relation is defined on Organization and Project, not on User
+    """
+    organizations = forms.ModelMultipleChoiceField(
+        queryset=Organization.objects.all(),
+        required=False)
+    projects = forms.ModelMultipleChoiceField(
+        queryset=Project.objects.all(),
+        required=False)
+
 class StoryUserAdmin(UserAdmin):
+    """
+    Custom admin for Users
+
+    Adds a filter in list view on Group name and uses the custom form
+    that allows editing of Organization and Project relations.
+    """
+    form = StoryUserAdminForm
+    # Add additional fieldsets so that our custom form fields are displayed
+    fieldsets = (
+        # These are the default fieldsets
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Groups'), {'fields': ('groups',)}),
+        # These are the custom fieldsets
+        (_('Organizations'), {'fields': ('organizations',)}),
+        (_('Projects'), {'fields': ('projects',)}),
+    )
+
     list_filter = UserAdmin.list_filter + ('groups__name',)
+
+    def save_model(self, request, obj, form, change):  
+        obj.organizations.clear()
+        obj.projects.clear()
+        for organization in form.cleaned_data['organizations']:
+            obj.organizations.add(organization)
+        for project in form.cleaned_data['projects']:
+            obj.projects.add(project)
+        obj.save()
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj:
+            self.form.base_fields['organizations'].initial = obj.organizations.all()
+            self.form.base_fields['projects'].initial = obj.projects.all()
+        return super(StoryUserAdmin, self).get_form(request, obj)
 
 admin.site.unregister(User)
 admin.site.register(User, StoryUserAdmin)
