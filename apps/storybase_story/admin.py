46f00f8ea405b7b11ea380117a7ab09bc3664b79
Geoffrey Hing <geoff@terrorware.com>
2012-03-12 11:30:52 -0500

    Added inline for assigning assets to a section.

--- apps/storybase_story/admin.py
+++ apps/storybase_story/admin.py
@@ -2,11 +2,7 @@ from django.contrib import admin
 from ajax_select import make_ajax_form
 from ajax_select.admin import AjaxSelectAdmin
 from storybase_asset.models import Asset
-from models import Story
-
-#class StoryAssetInline(admin.TabularInline):
-#    model = StoryAsset
-#    form = make_ajax_form(StoryAsset, {'asset': 'asset'})
+from models import Story, Section, SectionAsset
 
 class StoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
@@ -20,4 +16,24 @@ class StoryAdmin(admin.ModelAdmin):
             kwargs["queryset"] = Asset.objects.filter(owner=request.user)
         return super(StoryAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
+class SectionAssetInline(admin.TabularInline):
+    model = SectionAsset
+    # TODO: Fix this autocomplete
+    # It fails because the default autocomplete tries to to filter
+    # on the title of the Asset class which no longer exists after
+    # I moved that to the translations.
+    # See ajax_select.LookupChannel.get_query() to see where the 
+    # call to filter() that breaks things is.
+    # One solution might be to create a custom lookup class based
+    # on the Translation model instead of the Asset model.
+    # Another solution might be to add the Title field back to the
+    # Asset model and have the save hook of the translation update
+    # the related Asset's title.
+    #form = make_ajax_form(SectionAsset, dict(asset='asset'))
+    extra = 0
+
+class SectionAdmin(AjaxSelectAdmin):
+    inlines = [SectionAssetInline,]
+
 admin.site.register(Story, StoryAdmin)
+admin.site.register(Section, SectionAdmin)
