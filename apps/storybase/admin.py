fee92c46192e6f4800488c06dbcea13919e4c841
Geoffrey Hing <geoff@terrorware.com>
2012-03-15 10:20:58 -0500

    Added custom text field to allow unlimited length but with a text input instead of a text area widget.
    I put this in a newly created 'storybase' app that I imagine will hold
    common functionality shared across the other apps.

---
+++ apps/storybase/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.admin import widgets
+from fields import ShortTextField
+
+class AdminLongTextInputWidget(widgets.AdminTextInputWidget):
+    """ Slightly wider AdminTextInputWidget """
+    def __init__(self, attrs=None):
+        # Set size attribute as longer than default
+        # and override class so text input size doesn't get 
+        # shortened by CSS
+        final_attrs = {'size': 60, 'class': 'vLongTextField'}
+        if attrs is not None:
+            final_attrs.update(attrs)
+        super(AdminLongTextInputWidget, self).__init__(attrs=final_attrs)
+
+class StorybaseModelAdmin(admin.ModelAdmin):
+    """ Base class for ModelAdmin classes in this project 
+
+    Correctly sets the widget for our text field class in the Django
+    Admin.  It seems like this is the only place you can do it, though
+    it would be nice to do it with the field definition.
+
+    """
+    formfield_overrides = {
+        ShortTextField: {'widget': AdminLongTextInputWidget},
+    }
---
+++ apps/storybase/fields.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.db import models
+
+class ShortTextField(models.TextField):
+    """ The unlimited length of a TextField, but uses the CharField widget 
+    
+    There isn't any performance benefit to using varchar in Postgres, so
+    it makes sense to use text instead of using varchar with an arbitrary
+    charcter limit when the application doesn't call for one.
+
+    Still, in the Django admin or automatically-generated ModelForms, it's
+    nice to suggest to the user that this is a line of text rather than
+    a block of text, even if the length isn't actually limited.
+
+    See
+    http://www.postgresql.org/docs/8.4/interactive/datatype-character.html 
+
+    """
+    def formfield(self, **kwargs):
+        defaults = {'widget': forms.CharField}
+        defaults.update(kwargs)
+        print kwargs['widget']
+        return super(ShortTextField, self).formfield(**defaults)
