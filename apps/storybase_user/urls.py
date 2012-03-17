66d21b46f13928451032769fc16a435cc91dd0e3
Geoffrey Hing <geoff@terrorware.com>
2012-03-17 13:45:19 -0500

    Added bootstrap detail view for Project

--- apps/storybase_user/urls.py
+++ apps/storybase_user/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
-from views import OrganizationDetailView
+from views import OrganizationDetailView, ProjectDetailView
 
 urlpatterns = patterns('',
     url(r'organizations/(?P<organization_id>[0-9a-f]{32,32})/$', OrganizationDetailView.as_view(), name='organization_detail'), 
+    url(r'projects/(?P<project_id>[0-9a-f]{32,32})/$', ProjectDetailView.as_view(), name='project_detail'), 
 )
--- apps/storybase_user/views.py
+++ apps/storybase_user/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import DetailView
-from models import Organization
+from models import Organization, Project
 
 class OrganizationDetailView(DetailView):
 
@@ -8,4 +8,13 @@ class OrganizationDetailView(DetailView):
 
     def get_object(self):
         object = self.queryset.get(organization_id=self.kwargs['organization_id'])
+        return object
+
+class ProjectDetailView(DetailView):
+
+    context_object_name = "project"
+    queryset = Project.objects.all()
+
+    def get_object(self):
+        object = self.queryset.get(project_id=self.kwargs['project_id'])
         return object
