63a51393bd208405b0130c55a4149bc6a6427083
Geoffrey Hing <geoff@terrorware.com>
2012-03-17 13:26:43 -0500

    Fixed the unused import.  Previous commits/revers were me just being ignorant with git

--- apps/storybase_user/admin.py
+++ apps/storybase_user/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 from storybase.admin import StorybaseModelAdmin
 from models import Organization, Project
 
