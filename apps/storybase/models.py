705b58017be5e2433ad15962698ab1fef5835a53
Geoffrey Hing <geoff@terrorware.com>
2012-03-17 17:22:41 -0500

    Moved translation getter logic and APIish stuff into its own abstract base class

--- apps/storybase/models.py
+++ apps/storybase/models.py
@@ -1,3 +1,50 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
+from django.utils import translation
 
-# Create your models here.
+class TranslatedModel(models.Model):
+    translated_fields = []
+    _translation_cache = None
+
+    def __init__(self, *args, **kwargs):
+        super(TranslatedModel, self).__init__(*args, **kwargs)
+        self._translation_cache = {}
+
+    def __getattribute__(self, name):
+        """ Attribute getter that searches for fields on the translation model class
+
+        This implementation is based on the one in django-mothertongue by 
+        Rob Charlwood https://github.com/robcharlwood/django-mothertongue
+        """
+        get = lambda p:super(TranslatedModel, self).__getattribute__(p)
+        translated_fields = get('translated_fields') 
+        if name in translated_fields:
+            try:
+                translation_set = get('translation_set')
+                code = translation.get_language()
+                translated_manager = get(translation_set)
+                try:
+                    translated_object = None
+                    translated_object = self._translation_cache[code]
+                except KeyError:
+                    try:
+                        translated_object = translated_manager.get(language=code)
+                    except ObjectDoesNotExist:
+                        # If 'en-us' doesn't have a translation,
+                        # try 'en'
+                        new_code = code.split('-')[0]
+                        translated_object = translated_manager.get(language=new_code)
+                finally:
+                    self._translation_cache[code] = translated_object
+                if translated_object:
+                    return getattr(translated_object, name)
+            except (ObjectDoesNotExist, AttributeError):
+                # If title attribute doesn't exist on the Asset model, 
+                # try the subclass.
+                if name == 'title':
+                    return getattr(self.subclass(), name)
+
+        return get(name)
+
+    class Meta:
+        abstract = True
--- apps/storybase_asset/models.py
+++ apps/storybase_asset/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils.safestring import mark_safe
-from django.utils.translation import get_language, ugettext, ugettext_lazy as _
-from django.utils import translation
+from django.utils.translation import ugettext_lazy as _
 from filer.fields.image import FilerImageField
 from uuidfield.fields import UUIDField
+from storybase.models import TranslatedModel
 
 ASSET_TYPES = (
   (u'article', u'article'),
@@ -14,56 +14,19 @@ ASSET_TYPES = (
   (u'map', u'map'),
 )
 
-class Asset(models.Model):
+class Asset(TranslatedModel):
     asset_id = UUIDField(auto=True)
     type = models.CharField(max_length=10, choices=ASSET_TYPES)
     owner = models.ForeignKey(User, related_name="assets")
 
-    translated_fields = ('title', 'caption',)
-    _translation_cache = None
+    translated_fields = ['title', 'caption']
 
     def __unicode__(self):
         return self.title
 
     def __init__(self, *args, **kwargs):
         super(Asset, self).__init__(*args, **kwargs)
-        self._translation_cache = {}
 
-    def __getattribute__(self, name):
-        """ Attribute getter that searches for fields on the translation model class
-
-        This implementation is based on the one in django-mothertongue by 
-        Rob Charlwood https://github.com/robcharlwood/django-mothertongue
-        """
-        get = lambda p:super(Asset, self).__getattribute__(p)
-        translated_fields = get('translated_fields') 
-        if name in translated_fields:
-            try:
-                translation_set = get('translation_set')
-                code = translation.get_language()
-                translated_manager = get(translation_set)
-                try:
-                    translated_object = None
-                    translated_object = self._translation_cache[code]
-                except KeyError:
-                    try:
-                        translated_object = translated_manager.get(language=code)
-                    except ObjectDoesNotExist:
-                        # If 'en-us' doesn't have a translation,
-                        # try 'en'
-                        new_code = code.split('-')[0]
-                        translated_object = translated_manager.get(language=new_code)
-                finally:
-                    self._translation_cache[code] = translated_object
-                if translated_object:
-                    return getattr(translated_object, name)
-            except (ObjectDoesNotExist, AttributeError):
-                # If title attribute doesn't exist on the Asset model, 
-                # try the subclass.
-                if name == 'title':
-                    return getattr(self.subclass(), name)
-
-        return get(name)
 
     def subclass(self):
         for attr in ('externalasset', 'htmlasset', 'filerimageasset'):
@@ -96,7 +59,7 @@ class ExternalAsset(Asset):
     translations = models.ManyToManyField('ExternalAssetTranslation', blank=True, verbose_name=_('translations'))
 
     translation_set = 'storybase_asset_externalassettranslation_related'
-    translated_fields = Asset.translated_fields + ('url',)
+    translated_fields = Asset.translated_fields + ['url']
 
     def render_html(self):
         output = []
@@ -120,7 +83,7 @@ class HtmlAsset(Asset):
     translations = models.ManyToManyField('HtmlAssetTranslation', blank=True, verbose_name=_('translations'))
 
     translation_set = 'storybase_asset_htmlassettranslation_related'
-    translated_fields = Asset.translated_fields + ('body',)
+    translated_fields = Asset.translated_fields + ['body']
 
     def render_html(self):
         output = []
@@ -144,7 +107,7 @@ class FilerImageAsset(Asset):
     translations = models.ManyToManyField('FilerImageAssetTranslation', blank=True, verbose_name=_('translations'))
 
     translation_set = 'storybase_asset_filerimageassettranslation_related'
-    translated_fields = Asset.translated_fields + ('image',)
+    translated_fields = Asset.translated_fields + ['image']
 
     def render_html(self):
         output = []
