803ea4484aec88a2de97d9dfd7929a2f45035994
Geoffrey Hing <geoff@terrorware.com>
2012-03-21 15:33:29 -0500

    Unit tests for some of the new Model logic introduced in the last commit

--- apps/storybase_asset/tests.py
+++ apps/storybase_asset/tests.py
@@ -1,16 +1,31 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from models import get_asset, Asset, HtmlAsset, HtmlAssetTranslation
 
+class AssetModelTest(TestCase):
+    def test_subclass(self):
+        """ Test that Asset.subclass() returns the subclass of an Asset instance """
+        asset = HtmlAsset()
+        asset.save()
+        translation = HtmlAssetTranslation(title="Test", asset=asset)
+        translation.save()
+        retrieved_asset = Asset.objects.get(pk=asset.pk)
+        self.assertEqual(retrieved_asset.subclass(), asset)
+        self.assertEqual(retrieved_asset.subclass().__class__.__name__, 'HtmlAsset')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_license_name(self):
+        asset = HtmlAsset(license='CC BY')
+        asset.save()
+        translation = HtmlAssetTranslation(title="Test", asset=asset)
+        translation.save()
+        self.assertEqual(asset.license_name(), 'Attribution Creative Commons')
+
+class AssetApiTest(TestCase):
+    def test_get_asset_return_subclass(self):
+        """ Test that get_asset() returns the subclass of an Asset instance """
+        asset = HtmlAsset()
+        asset.save()
+        translation = HtmlAssetTranslation(title="Test", asset=asset)
+        translation.save()
+        retrieved_asset = get_asset(asset_id=asset.asset_id)
+        self.assertEqual(retrieved_asset, asset)
+        self.assertEqual(retrieved_asset.__class__.__name__, 'HtmlAsset')
