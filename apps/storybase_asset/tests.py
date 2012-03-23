8b11823dfc9bcd4eb09ffb1547add91b1ac3b3e0
Geoffrey Hing <geoff@terrorware.com>
2012-03-23 14:55:21 -0500

    Added unit test for Google Spreadsheet embedding

--- apps/storybase_asset/tests.py
+++ apps/storybase_asset/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from models import Asset, HtmlAsset, HtmlAssetTranslation
+from models import HtmlAsset, HtmlAssetTranslation
+from embedable_resource import EmbedableResource
 
 class AssetModelTest(TestCase):
     def test_license_name(self):
@@ -8,3 +9,9 @@ class AssetModelTest(TestCase):
         translation = HtmlAssetTranslation(title="Test", asset=asset)
         translation.save()
         self.assertEqual(asset.license_name(), 'Attribution Creative Commons')
+
+class EmbedableResourceTest(TestCase):
+    def test_get_google_spreadsheet_embed(self):
+        url = 'https://docs.google.com/spreadsheet/pub?key=0As2exFJJWyJqdDhBejVfN1RhdDg2b0QtYWR4X2FTZ3c&output=html' 
+        expected = "<iframe width='500' height='300' frameborder='0' src='%s&widget=true'></iframe>" % url
+        self.assertEqual(EmbedableResource.get_html(url), expected)
