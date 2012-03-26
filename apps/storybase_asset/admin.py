e425e58f06ca606b908e281956ecdca772f6a7d1
Geoffrey Hing <geoff@terrorware.com>
2012-03-26 11:35:06 -0500

    Enabled TinyMCE table plugin for Html Asset

--- apps/storybase_asset/admin.py
+++ apps/storybase_asset/admin.py
@@ -37,7 +37,7 @@ class HtmlAssetTranslationAdminForm(forms.ModelForm):
         widgets = {
                 'body': TinyMCE(
                     attrs={'cols': 80, 'rows': 30},
-                    mce_attrs={'theme': 'advanced', 'force_p_newlines': False, 'forced_root_block': '', 'theme_advanced_toolbar_location': 'top'},
+                    mce_attrs={'theme': 'advanced', 'force_p_newlines': False, 'forced_root_block': '', 'theme_advanced_toolbar_location': 'top', 'plugins': 'table', 'theme_advanced_buttons3_add': 'tablecontrols'},
                 )
         }
 
