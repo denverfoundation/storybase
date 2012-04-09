65074368c629e6ddae40e5ed2031c39dad18d018
Geoffrey Hing <geoff@terrorware.com>
2012-04-09 15:27:50 -0500

    Fixed typos, cleaned up indentation

--- apps/storybase_story/managers.py
+++ apps/storybase_story/managers.py
@@ -3,12 +3,12 @@
 from django.db import models
 
 class StoryManager(models.Manager):
-    """Add additional table-evel functionality to Story model"""
+    """Add additional table-level functionality to Story model"""
 
     def on_homepage(self):
         """Return stories to be featured on homepage"""
-	# While it's easy enough to just query the default manager
-	# to do this, providing this convenience method abstracts
-	# away the way homepage stories are designated in case
-	# we change the way that designation is done.
+        # While it's easy enough to just query the default manager
+        # to do this, providing this convenience method abstracts
+        # away the way homepage stories are designated in case
+        # we change the way that designation is done.
         return self.filter(on_homepage=True, status='published')
