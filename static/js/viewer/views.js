ccd1a1b9fff5965405f9a34b2df4b8c987f8ec66
Geoffrey Hing <geoff@terrorware.com>
2012-04-10 10:31:33 -0500

    Implemented Backbone view to change section header as user navigates through story with previous/next buttons.

--- static/js/viewer/views.js
+++ static/js/viewer/views.js
@@ -11,10 +11,11 @@ storybase.viewer.views.ViewerApp = Backbone.View.extend({
 
   initialize: function() {
     this.navigationView = new storybase.viewer.views.StoryNavigation(); 
+    this.headerView = new storybase.viewer.views.StoryHeader();
     this.sections = this.options.sections;
     this.story = this.options.story;
     this.currentSection = this.sections.at(0);
-    this.navigationView.setSection(this.currentSection);
+    this.updateSubviewSections();
   },
 
   render: function() {
@@ -23,13 +24,18 @@ storybase.viewer.views.ViewerApp = Backbone.View.extend({
     return this;
   },
 
+  updateSubviewSections: function() {
+    this.navigationView.setSection(this.currentSection);
+    this.headerView.setSection(this.currentSection);
+  },
+
   nextSection: function() {
     //console.debug('Clicked next');
     //console.debug('Current section is ' + this.currentSection.id);
     var nextId = this.currentSection.get('next_section_id');
     if (nextId) {
       this.currentSection = this.sections.get(nextId);
-      this.navigationView.setSection(this.currentSection);
+      this.updateSubviewSections();
       //console.debug('New section is ' + this.currentSection.id);
     }
   },
@@ -40,15 +46,28 @@ storybase.viewer.views.ViewerApp = Backbone.View.extend({
     var prevId = this.currentSection.get('previous_section_id');
     if (prevId) {
       this.currentSection = this.sections.get(prevId);
-      this.navigationView.setSection(this.currentSection);
+      this.updateSubviewSections();
       //console.debug('New section is ' + this.currentSection.id);
     }
   }
 });
 
 storybase.viewer.views.StoryHeader = Backbone.View.extend({
+  el: 'header',
+
   render: function() {
+    var $titleEl = this.$el.find('.section-title').first();
+    if ($titleEl.length == 0) {
+      $titleEl = $('<h2 class="section-title">');
+      this.$el.append($titleEl);
+    }
+    $titleEl.text(this.section.get('title'));
     return this;
+  },
+
+  setSection: function(section) {
+    this.section = section;
+    this.render();
   }
 });
 
