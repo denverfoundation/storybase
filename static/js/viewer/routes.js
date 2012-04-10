35068dc568183e8fe9e1696372d00cc7d7005908
Geoffrey Hing <geoff@terrorware.com>
2012-04-10 11:14:41 -0500

    Use router for each section.
    That way we can just use normal links and not event handlers for the
    previouse and next control.

--- static/js/viewer/routes.js
+++ static/js/viewer/routes.js
@@ -2,8 +2,16 @@
  * Routes for the story viewer Backbone application.
  */
 Namespace('storybase.viewer');
-storybase.viewer.Router = Backbone.Router.extend({
+storybase.viewer.routers.Router = Backbone.Router.extend({
   routes: {
-    "section/:id": "section"
+    "sections/:id": "section"
+  },
+
+  initialize: function(options) {
+    this.view = options.view;
+  },
+
+  section: function(id) {
+    this.view.setSectionById(id);
   }
-});
+})
--- static/js/viewer/views.js
+++ static/js/viewer/views.js
@@ -5,8 +5,6 @@ Namespace('storybase.viewer');
 
 storybase.viewer.views.ViewerApp = Backbone.View.extend({
   events: {
-    "click nav .next"     : "nextSection",
-    "click nav .previous" : "prevSection"
   },
 
   initialize: function() {
@@ -14,8 +12,7 @@ storybase.viewer.views.ViewerApp = Backbone.View.extend({
     this.headerView = new storybase.viewer.views.StoryHeader();
     this.sections = this.options.sections;
     this.story = this.options.story;
-    this.currentSection = this.sections.at(0);
-    this.updateSubviewSections();
+    this.setSection(this.sections.at(0));
   },
 
   render: function() {
@@ -29,27 +26,15 @@ storybase.viewer.views.ViewerApp = Backbone.View.extend({
     this.headerView.setSection(this.currentSection);
   },
 
-  nextSection: function() {
-    //console.debug('Clicked next');
-    //console.debug('Current section is ' + this.currentSection.id);
-    var nextId = this.currentSection.get('next_section_id');
-    if (nextId) {
-      this.currentSection = this.sections.get(nextId);
-      this.updateSubviewSections();
-      //console.debug('New section is ' + this.currentSection.id);
-    }
+  setSection: function(section) {
+    this.currentSection = section;
+    this.updateSubviewSections();
   },
 
-  prevSection: function() {
-    //console.debug('Clicked previous');
-    //console.debug('Previous section is ' + this.currentSection.id);
-    var prevId = this.currentSection.get('previous_section_id');
-    if (prevId) {
-      this.currentSection = this.sections.get(prevId);
-      this.updateSubviewSections();
-      //console.debug('New section is ' + this.currentSection.id);
-    }
-  }
+  setSectionById: function(id) {
+    console.debug('got here');
+    this.setSection(this.sections.get(id));
+  },
 });
 
 storybase.viewer.views.StoryHeader = Backbone.View.extend({
