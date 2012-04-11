8fe549935dcde00eb9400b04b9714a7a5eaf9904
Geoffrey Hing <geoff@terrorware.com>
2012-04-11 11:23:03 -0500

    Added ability to set Google Analytics domain in tracking code

--- apps/storybase/context_processors.py
+++ apps/storybase/context_processors.py
@@ -5,7 +5,7 @@ from django.conf import settings
 def conf(request):
     """Add selected settings values to the template context"""
     context = {}
-    for setting in ('GA_PROPERTY_ID', 'ADDTHIS_PUBID'):
+    for setting in ('ADDTHIS_PUBID', 'GA_PROPERTY_ID', 'GA_DOMAIN_NAME'):
         setting_val = getattr(settings, setting, None)
         if setting_val is not None:
             context.update({setting.lower(): setting_val})
