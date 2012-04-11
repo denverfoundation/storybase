2a719a3ed4182073ab33e96b949a6d0a100c11fa
Geoffrey Hing <geoff@terrorware.com>
2012-04-11 09:41:53 -0500

    Made conf context processor have less repeated code, added setting for AddThis profile ID

--- apps/storybase/context_processors.py
+++ apps/storybase/context_processors.py
@@ -5,8 +5,9 @@ from django.conf import settings
 def conf(request):
     """Add selected settings values to the template context"""
     context = {}
-    ga_property_id = getattr(settings, 'GA_PROPERTY_ID', None)
-    if ga_property_id is not None:
-        context.update({'ga_property_id': ga_property_id})
+    for setting in ('GA_PROPERTY_ID', 'ADDTHIS_PUBID'):
+        setting_val = getattr(settings, setting, None)
+        if setting_val is not None:
+            context.update({setting.lower(): setting_val})
 
     return context
