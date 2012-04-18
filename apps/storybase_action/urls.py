a104861a4a16a4c36d3a567fb67b7de7449d84ab
Geoffrey Hing <geoff@terrorware.com>
2012-04-18 08:18:35 -0500

    Made contact form path absolute.  It was matching in the admin.

--- apps/storybase_action/urls.py
+++ apps/storybase_action/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.defaults import patterns, url
 from storybase_action.views import SiteContactMessageCreateView
 
 urlpatterns = patterns('',
-    url(r'contact/$',
+    url(r'^contact/$',
         SiteContactMessageCreateView.as_view(), name='contact'), 
 )
