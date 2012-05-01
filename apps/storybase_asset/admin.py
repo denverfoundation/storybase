abd1c64ffc4b4f15eba3f8575a1a15b75c43b591
Geoff Hing <geoffhing@gmail.com>
2012-05-01 13:22:45 -0500

    Added editor resizing control to TinyMCE

--- apps/storybase_asset/admin.py
+++ apps/storybase_asset/admin.py
@@ -71,7 +71,7 @@ class HtmlAssetTranslationAdminForm(forms.ModelForm):
         widgets = {
                 'body': TinyMCE(
                     attrs={'cols': 80, 'rows': 30},
-                    mce_attrs={'theme': 'advanced', 'force_p_newlines': False, 'forced_root_block': '', 'theme_advanced_toolbar_location': 'top', 'plugins': 'table', 'theme_advanced_buttons3_add': 'tablecontrols'},
+		    mce_attrs={'theme': 'advanced', 'force_p_newlines': False, 'forced_root_block': '', 'theme_advanced_toolbar_location': 'top', 'plugins': 'table', 'theme_advanced_buttons3_add': 'tablecontrols', 'theme_advanced_statusbar_location': 'bottom', 'theme_advanced_resizing' : True},
                 )
         }
 
