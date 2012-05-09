b6c28e587a43591ad39a79c07cdc2a844e58f899
Geoff Hing <geoffhing@gmail.com>
2012-05-09 17:27:43 -0500

    Add projects and organizations to menu

--- apps/cmsplugin_storybase/menu.py
+++ apps/cmsplugin_storybase/menu.py
@@ -6,6 +6,8 @@ from menus.menu_pool import menu_pool
 
 from cmsplugin_storybase import settings as plugin_settings
 
+from storybase_user.models import Organization, Project
+
 class StorybaseMenu(Menu):
     """
     Add views from the various StoryBase apps to the CMS menu
@@ -21,11 +23,26 @@ class StorybaseMenu(Menu):
 			          id='projects',
 				  parent_id=explore.id)
 	nodes.append(projects)
+	for project in Project.objects.all():
+            nodes.append(
+	        NavigationNode(
+	            title=project.name,
+		    url=project.get_absolute_url(),
+		    id=project.project_id,
+		    parent_id=projects.id))
 	organizations = NavigationNode(title=_("Organizations"),
                                        url=reverse('organization_list'),
 				       id='organizations',
 				       parent_id=explore.id)
 	nodes.append(organizations)
+	for organization in Organization.objects.all():
+            nodes.append(
+	        NavigationNode(
+	            title=organization.name,
+		    url=organization.get_absolute_url(),
+		    id=organization.organization_id,
+		    parent_id=organizations.id))
+                                        
         return nodes
 
 menu_pool.register_menu(StorybaseMenu)
@@ -40,16 +57,20 @@ class OrderMenuNodes(Modifier):
         explore_node = None
         explore_nodes = []
         insert_at = plugin_settings.STORYBASE_EXPLORE_MENU_POSITION
-	# Dummy check in case the explore menu
+	# Check to make sure we're trying to insert the explore menu nodes
+	# in a position that actually exists
 	insert_at = insert_at if insert_at < len(nodes) else len(nodes) - 1
         for node in nodes:
+           # Discover nodes in the explore menu and separate them
            if node.id == 'explore':
                 explore_node = node
                 explore_nodes.append(node)	
-           elif node in explore_node.children:
+	   elif explore_node in node.get_ancestors():
 	        explore_nodes.append(node)	
            else:
                 modified_nodes.append(node)
+
+	# Add the explore nodes back at the specified position
         for node in explore_nodes:
             modified_nodes.insert(insert_at, node) 
             insert_at += 1
