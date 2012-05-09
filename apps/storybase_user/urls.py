50f3e7a05f6533c085af03172e7cfec92c1b69d2
Geoff Hing <geoffhing@gmail.com>
2012-05-09 14:36:29 -0500

    Added skeleton for organization and project list views.

--- apps/storybase_user/urls.py
+++ apps/storybase_user/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls.defaults import *
-from views import OrganizationDetailView, ProjectDetailView
+from storybase_user.views import (OrganizationDetailView, OrganizationListView,
+                                  ProjectDetailView, ProjectListView)
 
 urlpatterns = patterns('',
+    url(r'organizations/$', OrganizationListView.as_view(),
+	name='organization_list'),
     url(r'organizations/(?P<organization_id>[0-9a-f]{32,32})/$',
 	OrganizationDetailView.as_view(), name='organization_detail_by_id'), 
     url(r'organizations/(?P<slug>[0-9a-z-]+)/$',
         OrganizationDetailView.as_view(), name='organization_detail'), 
+    url(r'projects/$', ProjectListView.as_view(), name='project_list'),
     url(r'projects/(?P<project_id>[0-9a-f]{32,32})/$',
 	ProjectDetailView.as_view(), name='project_detail_by_id'), 
     url(r'projects/(?P<slug>[0-9a-z-]+)/$',
--- apps/storybase_user/views.py
+++ apps/storybase_user/views.py
@@ -1,15 +1,28 @@
 """Views"""
 
+from django.views.generic.list import ListView
+
 from storybase.views.generic import ModelIdDetailView
 from storybase_user.models import Organization, Project
 
 class OrganizationDetailView(ModelIdDetailView):
-
+    """Display details about an Organization"""
     context_object_name = "organization"
     queryset = Organization.objects.all()
 
 
-class ProjectDetailView(ModelIdDetailView):
+class OrganizationListView(ListView):
+    """Display a list of all Organizations"""
+    context_object_name = 'organizations'
+    queryset = Organization.objects.all()
 
+
+class ProjectDetailView(ModelIdDetailView):
+    """Display details about a Project"""
     context_object_name = "project"
+    queryset = Project.objects.all()
+
+class ProjectListView(ListView):
+    """Display a list of all Projects"""
+    context_object_name = "projects"
     queryset = Project.objects.all()
