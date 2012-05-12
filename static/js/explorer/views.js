c30bf811a657566931253e89bba72336be778585
Geoff Hing <geoffhing@gmail.com>
2012-05-12 16:39:48 -0500

    Use a lambda in the mustache template for translations so we don't have to pass translated strings as arguments.

--- static/js/explorer/views.js
+++ static/js/explorer/views.js
@@ -6,22 +6,17 @@ Namespace('storybase.explorer');
 storybase.explorer.views.Filters = Backbone.View.extend({
   el: $('#filters'),
 
-  render: function(options) {
-    var context;
-    // Check to see if this method was passed extra template context
-    // The main reason for this is to be able to define translated strings
-    // in the template rather than the view.
-    if (typeof options === "undefined" || 
-        typeof options.extraContext === "undefined") {
-      context = {};
-    }
-    else {
-      context = options.extraContext;
-    }
+  render: function() {
+    var context = {};
     context.topics = this.options.topics;
     context.organizations = this.options.organizations;
     context.projects = this.options.projects;
     context.languages = this.options.languages;
+    context.gettext = function() {
+      return function(text) {
+        return gettext(text);
+      }
+    };
     this.$el.html(ich.filtersTemplate(context));
   }
 });
