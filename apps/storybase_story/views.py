a0409ac0681c4257536c065eccd2853f086f2354
Geoff Hing <geoffhing@gmail.com>
2012-05-16 10:10:07 -0500

    Allow initial filtering of explore view based on querystring parameters
    The querystring parameters just get passed on to ``StoryResource``

--- apps/storybase_story/views.py
+++ apps/storybase_story/views.py
@@ -1,5 +1,7 @@
 """Views for storybase_story app"""
 
+import json
+
 from django.template import Context
 from django.template.loader import get_template
 from django.views.generic import TemplateView 
@@ -27,14 +29,39 @@ def homepage_story_list():
 
 
 class ExploreStoriesView(TemplateView):
+    """
+    A view that lets users filter a list of stories
+
+    The heavy lifting is done by ```StoryResource``` and client-side 
+    JavaScript.
+
+    """
     template_name = "storybase_story/explore_stories.html"
+
+    def _get_selected_filters(self):
+        """
+	Build a data structure of selected filters
+	
+	The selected filters are based on the parameters passed in request.GET
+
+	The returned data structure is meant to be serialized and passed to
+	client-side code.
+
+	"""
+	selected_filters = {}
+	for filter in StoryResource._meta.explore_filter_fields:
+	  values = self.request.GET.get(filter, None)
+	  if values:
+	    selected_filters[filter] = values.split(",")
+	return selected_filters
 
     def get_context_data(self, **kwargs):
         resource = StoryResource()
-	to_be_serialized = resource.explore_get_data_to_be_serialized()
+	to_be_serialized = resource.explore_get_data_to_be_serialized(self.request)
 	return {
             'stories_json': mark_safe(resource.serialize(None, to_be_serialized,
-                                                         'application/json'))
+                                                         'application/json')),
+	    'selected_filters': mark_safe(json.dumps(self._get_selected_filters()))
 	}
 
 
