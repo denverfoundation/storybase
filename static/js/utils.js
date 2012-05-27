7c8eeee9bb3e1fea6d8b8ee0e36c90a25a740435
Geoffrey Hing <geoff@terrorware.com>
2012-05-27 12:09:44 -0500

    Implemented block translating in Handlebars

--- static/js/utils.js
+++ static/js/utils.js
@@ -4,14 +4,35 @@
 
 Namespace('storybase.utils');
 
+/**
+ * Translate a string in a template using Django's gettext Javascript API.
+ * @param {object} options Hash of options. Should contain one key,
+ *     'message' which is the string to be translated.
+ * @return {string} Translated string
+ */
 Handlebars.registerHelper('gettext', function(options) {
   return gettext(options.hash.message);
 });
 
 /**
- * Pluralize a string
- *
- * Works in manner similar to Django's pluralize filter
+ * Translate a block of template text after first rendering the
+ * contents of the template.
+ * @return {string} Translated block of text
+ */
+Handlebars.registerHelper('blockgettext', function(options) {
+  return gettext(options.fn(this));
+});
+
+/**
+ * Return a singular or plural suffix
+ * Works in a manner similar to Django's pluralize filter
+ * @param {number} count The number used to determine if the singular or
+ *    plural suffix should be returned.
+ * @param {object} options Hash of options. Optionally contains one key,
+ *     'suffix' which is either the plural suffix (e.g. 's') or a
+ *     comma-separated list of the singular and plural suffixes
+ *     (e.g. 'y,ies')
+ * @return {string} Suffix
  */
 Handlebars.registerHelper('pluralize', function(count, options) {
   var suffix;
