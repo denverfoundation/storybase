4107fe1240598f64335bf3203c717f4ef00f9e0f
Geoff Hing <geoffhing@gmail.com>
2012-05-30 07:59:15 -0500

    Switch to postgis for Travis
    At least until I have time to figure out the spatialite automatic build.

--- settings/travis.py
+++ settings/travis.py
@@ -3,10 +3,10 @@ from defaults import *
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': 'atlas_test',
-        #'USER': 'postgres',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'atlas_test',
+        'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
