52520273335869efb2f46235fcfcccd27e8c54c6
Geoffrey Hing <geoff@terrorware.com>
2012-05-29 19:52:26 -0500

    Added copyright notice to code copied from Django distribution

--- apps/storybase/templatetags/storybase_i18n.py
+++ apps/storybase/templatetags/storybase_i18n.py
@@ -3,6 +3,41 @@ import django
 if django.VERSION[0] <=1 and django.VERSION[2] < 4: 
     # Make the ``langauge`` template tag available for Django versions earlier
     # than 1.4
+
+    # Copyright (c) Django Software Foundation and individual contributors.
+    # All rights reserved.
+    #
+    # Redistribution and use in source and binary forms, with or without
+    # modification, are permitted provided that the following conditions 
+    # are met:
+    # 
+    #
+    #    1. Redistributions of source code must retain the above copyright 
+    #       notice, this list of conditions and the following disclaimer.
+    #      
+    #    
+    #    2. Redistributions in binary form must reproduce the above copyright 
+    #       notice, this list of conditions and the following disclaimer in the
+    #       documentation and/or other materials provided with the 
+    #       distribution.
+    #
+    #    3. Neither the name of Django nor the names of its contributors may 
+    #       be used to endorse or promote products derived from this software 
+    #       without specific prior written permission.
+    #      
+    #
+    # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+    # IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+    # TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+    # PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER 
+    # OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+    # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+    # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+    # PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+    # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  (INCLUDING   
+    # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   
     from django.template import (Node, Library, TemplateSyntaxError)
     from django.utils.translation import (activate, deactivate, deactivate_all,
         get_language)
@@ -51,7 +86,8 @@ if django.VERSION[0] <=1 and django.VERSION[2] < 4:
         """
         bits = token.split_contents()
         if len(bits) != 2:
-            raise TemplateSyntaxError("'%s' takes one argument (language)" % bits[0])
+            raise TemplateSyntaxError("'%s' takes one argument (language)" %
+                                      bits[0])
         language = parser.compile_filter(bits[1])
         nodelist = parser.parse(('endlanguage',))
         parser.delete_first_token()
