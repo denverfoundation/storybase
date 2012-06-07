d27a50532146174814bc48b7afe6b357dbcd7b93
Geoff Hing <geoffhing@gmail.com>
2012-06-07 13:25:42 -0500

    Added templates for password reset flow

--- apps/storybase_user/account_urls.py
+++ apps/storybase_user/account_urls.py
@@ -7,10 +7,12 @@ at a different root than the other URL patterns.
 """
 
 from django.conf.urls.defaults import *
+from django.contrib.auth.views import password_reset
 
 from storybase_user.views import AccountSummaryView
 
 urlpatterns = patterns('',
     url(r'^$', AccountSummaryView.as_view(),
 	name='account_summary'),
+    url(r'^password/reset$', password_reset, name='password_reset'),
 )
