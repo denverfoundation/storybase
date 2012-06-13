e7e6664d1a9b764aab626a7b36ecd5259ed38436
Geoff Hing <geoffhing@gmail.com>
2012-06-12 23:58:44 -0500

    Prevent users from associating their Twitter account with an existing one

--- apps/storybase_user/social_auth/forms.py
+++ apps/storybase_user/social_auth/forms.py
@@ -1,5 +1,6 @@
 """Custom forms for authentication and registration"""
 from django import forms
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 # TODO: See if this can be merged with 
@@ -13,3 +14,12 @@ class TosForm(forms.Form):
 
 class EmailTosForm(TosForm):
     email = forms.EmailField()
+
+    def clean_email(self):
+        """
+        Validate that the supplied email address is unique for the site.
+        """
+        if User.objects.filter(email__iexact=self.cleaned_data['email']):
+            raise forms.ValidationError(_("This email address is already in use. You must first log in to your account with your password and then associate your social media account."))
+        return self.cleaned_data['email']
+
