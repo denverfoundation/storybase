34a826b8a23159d13a449970bfecdd9efe7fe4da
Geoff Hing <geoffhing@gmail.com>
2012-06-15 09:23:15 -0500

    Added post_syncdb signal handler to create the admin group when setting it up

--- apps/storybase_user/management/__init__.py
+++ apps/storybase_user/management/__init__.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth.models import User, Group
 from django.db.models.signals import post_syncdb
 
 # Based on django-email-usernames by Hakan Waara
@@ -25,3 +26,22 @@ def increase_username_and_email_length(sender, app, created_models, verbosity,
             break
 
 post_syncdb.connect(increase_username_and_email_length)
+
+
+def create_admin_group(sender, app, created_models, verbosity, interactive,
+                       **kwargs):
+    if (interactive and app.__name__ == 'django.contrib.auth.models' and
+        Group not in created_models):
+        try:
+            admin_group = Group.objects.get(name=settings.ADMIN_GROUP_NAME)
+        except Group.DoesNotExist:
+            msg = ("\nCreate administrator group? (yes/no): ")
+            confirm = raw_input(msg)
+            while 1:
+                if confirm not in ('yes', 'no'):
+                    confirm = raw_input('Please enter either "yes" or "no": ')
+                    continue
+                if confirm == 'yes':
+                    Group.objects.create(name=settings.ADMIN_GROUP_NAME)    
+                break
+post_syncdb.connect(create_admin_group)
