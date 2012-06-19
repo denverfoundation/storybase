89e57b4daafeea77040072deecd4024eeb10ee17
Geoff Hing <geoffhing@gmail.com>
2012-06-18 22:19:46 -0500

    Added template tag for accessing storybase configuration values.

--- apps/storybase/tests/__init__.py
+++ apps/storybase/tests/__init__.py
@@ -16,3 +16,14 @@ class ContextProcessorTest(TestCase):
         c = RequestContext(req)
         self.assertEqual(t.render(c), contact_email)
 
+class TemplateTagTest(TestCase):
+    def test_storybase_conf(self):
+        """Test the storybase_conf template tag"""
+        from django.conf import settings
+        from django.template import Context, Template
+        contact_email = "contactme@example.com"
+        settings.STORYBASE_CONTACT_EMAIL = contact_email 
+        t = Template("{% load storybase_tags %}{% storybase_conf \"storybase_contact_email\"%}")
+        c = Context()
+        self.assertEqual(t.render(c), contact_email)
+
