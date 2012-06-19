278314ff2a1e9b793d04f04d7ad72e4d8c4e45fd
Geoff Hing <geoffhing@gmail.com>
2012-06-18 23:55:48 -0500

    Added required CSS class to form

--- apps/storybase_user/registration/forms.py
+++ apps/storybase_user/registration/forms.py
@@ -17,6 +17,8 @@ class EmailUsernameRegistrationForm(RegistrationFormUniqueEmail):
     except ImportError:
         pass
 
+    required_css_class = 'required'
+
     def __init__(self, *args, **kwargs):
         super(EmailUsernameRegistrationForm, self).__init__(*args, **kwargs)
         # Remove the username field
@@ -35,5 +37,7 @@ class ExtraInfoRegistrationForm(EmailUsernameRegistrationForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30, required=False)
     tos = forms.BooleanField(widget=forms.CheckboxInput(),
-                             label=_(u'I have read and agree to the Terms of Service'),
+                             label=_(u'I have agree to the terms of service'),
                              error_messages={'required': _("You must agree to the terms to register")})
+
+    required_css_class = 'required'
