103440a2fdb280666d27c69e2e99d56c1b5c07ae
Geoff Hing <geoffhing@gmail.com>
2012-06-18 21:34:44 -0500

    Fixed context preprocessor test

--- apps/storybase/tests/__init__.py
+++ apps/storybase/tests/__init__.py
@@ -4,10 +4,15 @@ class ContextProcessorTest(TestCase):
     def test_conf(self):
         """Test the conf context preprocessor"""
         from django.conf import settings
+        from django.http import HttpRequest
         from django.template import Template, RequestContext
+        from django.contrib.auth.models import AnonymousUser
         contact_email = "contactme@example.com"
         settings.STORYBASE_CONTACT_EMAIL = contact_email 
         t = Template("{{ storybase_contact_email }}")
-        c = RequestContext()
+        user = AnonymousUser()
+        req = HttpRequest()
+        req.user = user
+        c = RequestContext(req)
         self.assertEqual(t.render(c), contact_email)
 
