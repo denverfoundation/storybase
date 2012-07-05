ef5731a3e4d621ea6ec449f8ff171f5932131018
Geoffrey Hing <geoff@terrorware.com>
2012-07-05 13:00:35 -0500

    Added routing for detail endpoint and added caption field to resource

--- apps/storybase_asset/api.py
+++ apps/storybase_asset/api.py
@@ -1,6 +1,8 @@
+from django.conf.urls.defaults import url
 
 from tastypie import fields
 from tastypie.authentication import Authentication
+from tastypie.bundle import Bundle
 
 from storybase.api import (DelayedAuthorizationResource,
     TranslatedModelResource, LoggedInAuthorization)
@@ -10,8 +12,10 @@ class AssetResource(DelayedAuthorizationResource, TranslatedModelResource):
     # Explicitly declare fields that are on the translation model, or the
     # subclass
     title = fields.CharField(attribute='title', null=True)
+    caption = fields.CharField(attribute='caption', null=True)
     body = fields.CharField(attribute='body', null=True)
     url = fields.CharField(attribute='url', null=True)
+    image = fields.FileField(attribute='image', null=True)
 
     class Meta:
         queryset = Asset.objects.select_subclasses()
@@ -37,3 +41,24 @@ class AssetResource(DelayedAuthorizationResource, TranslatedModelResource):
             q = q | Q(owner=request.user)
 
         return super(AssetResource, self).get_object_list(request).filter(q)
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<asset_id>[0-9a-f]{32,32})/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
+
+    def detail_uri_kwargs(self, bundle_or_obj):
+        """
+        Given a ``Bundle`` or an object (typically a ``Model`` instance),
+        it returns the extra kwargs needed to generate a detail URI.
+
+        This version returns the asset_id field instead of the pk
+        """
+        kwargs = {}
+
+        if isinstance(bundle_or_obj, Bundle):
+            kwargs[self._meta.detail_uri_name] = bundle_or_obj.obj.asset_id
+        else:
+            kwargs[self._meta.detail_uri_name] = bundle_or_obj.asset_id
+
+        return kwargs
