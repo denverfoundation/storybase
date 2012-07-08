eb86e1580e900d2f209bdd2f4961078e5481f19a
Geoffrey Hing <geoff@terrorware.com>
2012-07-08 09:20:23 -0500

    Moved Section model definition out of the viewer app and into the central model definition

--- static/js/models.js
+++ static/js/models.js
@@ -21,3 +21,24 @@ storybase.collections.Stories = Backbone.Collection.extend({
       return '/api/0.1/stories';
     }
 });
+
+storybase.models.Section = Backbone.Model.extend({
+  idAttribute: "section_id",
+
+  populateChildren: function() {
+    var $this = this;
+    this.children = this.get('children').map(function(childId) {
+      return $this.collection.get(childId).populateChildren();
+    });
+    return this;
+  },
+
+  title: function() {
+    return this.get("title");
+  }
+});
+
+storybase.collections.Sections = Backbone.Collection.extend({
+    model: storybase.models.Section
+});
+ 
