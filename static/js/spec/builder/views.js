27cefdac4d36a617dfe5afc6b139e8464ffe2469
Geoffrey Hing <geoff@terrorware.com>
2012-07-13 13:54:33 -0500

    Scaffolding to test rendering of builder Backbone views.

--- static/js/spec/builder/views.js
+++ static/js/spec/builder/views.js
@@ -1,10 +1,23 @@
 describe('SectionAssetEditView view', function() {
   beforeEach(function() {
-    this.view = new storybase.builder.views.SectionAssetEditView({});
+    jasmine.getFixtures().fixturesPath = 'spec/fixtures';
+    loadFixtures('builder_templates.html');
+    this.assetTypes = [
+      {
+        'name': 'image',
+        'type': 'image'
+      },
+      {
+        'name': 'text',
+        'type': 'text'
+      }
+    ];
+    this.view = new storybase.builder.views.SectionAssetEditView({
+       assetTypes: this.assetTypes,
+    });
   });
 
   describe('when initializing', function() {
-
     it('should have an undefined model property', function() {
       expect(this.view.model).toBeUndefined();
     });
@@ -13,4 +26,17 @@ describe('SectionAssetEditView view', function() {
       expect(this.view.type).toBeUndefined();
     });
   });
+
+  describe('when rendering', function(){
+    beforeEach(function() {
+      this.view.render();
+    });
+
+    describe('with an undefined type property', function() {
+      it('should provide a list of available asset types', function() {
+        expect(_.size(this.view.$('.asset-type'))).toEqual(_.size(this.assetTypes));
+      });
+    });
+  });
+
 });
