524a07e9728af2de0950dc46f5adfc4f957321d6
Geoffrey Hing <geoff@terrorware.com>
2012-07-14 10:26:04 -0500

    Broke handlebars templates out into a separate file so I can use them for testing with Jasmine

--- static/js/spec/builder/views.js
+++ static/js/spec/builder/views.js
@@ -1,7 +1,8 @@
 describe('SectionAssetEditView view', function() {
   beforeEach(function() {
+    // Load Handlebars templates from a fixture file
     jasmine.getFixtures().fixturesPath = 'spec/fixtures';
-    loadFixtures('builder_templates.html');
+    loadFixtures('story_builder_handlebars.html');
     this.assetTypes = [
       {
         'name': 'image',
@@ -37,6 +38,63 @@ describe('SectionAssetEditView view', function() {
         expect(_.size(this.view.$('.asset-type'))).toEqual(_.size(this.assetTypes));
       });
     });
+
+    describe('with the type property set', function() {
+      it('should provide a form for selecting an asset', function() {
+        this.view.type = 'text';
+        this.view.render();
+        expect(this.view.$el).toContain('form');
+      });
+
+      describe('with a type of "image"', function() {
+        beforeEach(function() {
+          this.view.type = 'image';
+          this.view.render();
+        });
+
+        it('should provide a file and url input', function() {
+          expect(this.view.$el).toContain('input[name="url"]');
+          expect(this.view.$el).toContain('input[name="file"]');
+        });
+
+        it('should not include a textarea', function() {
+          expect(this.view.$el).not.toContain('textarea');
+        });
+      });
+
+      describe('with a type of "text"', function() {
+        beforeEach(function() {
+          this.view.type = 'text';
+          this.view.render();
+        });
+
+        it('should not provide a file and url input', function() {
+          expect(this.view.$el).not.toContain('input[name="url"]');
+          expect(this.view.$el).not.toContain('input[name="file"]');
+        });
+
+        it('should include a textarea', function() {
+          expect(this.view.$el).toContain('textarea');
+        });
+      });
+    });
   });
 
+  describe('with the model property set', function() {
+    describe('with the model type of "text"', function() {
+      beforeEach(function() {
+         this.view.model = new Backbone.Model({
+           id: 'bef53407591f4fd8bd169f9cc02672f9',
+           type: 'text',
+           body: 'Test text asset body',
+           content: 'Test text asset body'
+         });
+         this.view.render();
+      });
+
+      it("should display the model's body", function() {
+        expect(this.view.$el.text()).toContain(this.view.model.get('body'));
+      });
+    });
+  });
 });
