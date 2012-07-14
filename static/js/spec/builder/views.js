1ea189c18c5068a8dadd7211d8f06b3a70e4f2ba
Geoffrey Hing <geoff@terrorware.com>
2012-07-14 11:48:34 -0500

    Reorganized tests

--- static/js/spec/builder/views.js
+++ static/js/spec/builder/views.js
@@ -32,68 +32,83 @@ describe('SectionAssetEditView view', function() {
     });
   });
 
-  describe('when rendering', function(){
-    beforeEach(function() {
-      this.view.render();
-    });
+  describe('with an undefined type property', function() {
+    describe('when rendering', function(){
+      beforeEach(function() {
+        this.view.render();
+      });
 
-    describe('with an undefined type property', function() {
       it('should provide a list of available asset types', function() {
         expect(_.size(this.view.$('.asset-type'))).toEqual(_.size(this.assetTypes));
       });
     });
+  });
 
-    describe('with the type property set', function() {
-      it('should provide a form for selecting an asset', function() {
-        this.view.type = 'text';
+
+  describe('with the type property set to "text"', function() {
+    beforeEach(function() {
+      this.view.type = 'text';
+    });
+
+    describe('when rendering', function(){
+      beforeEach(function() {
         this.view.render();
+      });
+
+      it('should provide a form for selecting an asset', function() {
         expect(this.view.$el).toContain('form');
       });
 
-      describe('with a type of "image"', function() {
-        beforeEach(function() {
-          this.view.type = 'image';
-          this.view.render();
-        });
-
-        it('should provide a file and url input', function() {
-          expect(this.view.$el).toContain('input[name="url"]');
-          expect(this.view.$el).toContain('input[name="file"]');
-        });
-
-        it('should not include a textarea', function() {
-          expect(this.view.$el).not.toContain('textarea');
-        });
+      it('should not provide a file and url input', function() {
+        expect(this.view.$el).not.toContain('input[name="url"]');
+        expect(this.view.$el).not.toContain('input[name="file"]');
       });
 
-      describe('with a type of "text"', function() {
-        beforeEach(function() {
-          this.view.type = 'text';
-          this.view.render();
-        });
-
-        it('should not provide a file and url input', function() {
-          expect(this.view.$el).not.toContain('input[name="url"]');
-          expect(this.view.$el).not.toContain('input[name="file"]');
-        });
-
-        it('should include a textarea', function() {
-          expect(this.view.$el).toContain('textarea');
-        });
+      it('should include a textarea', function() {
+        expect(this.view.$el).toContain('textarea');
       });
     });
   });
 
-  describe('with the model property set', function() {
-    describe('with the model type of "text"', function() {
+  describe('with the type property set to "image"', function() {
+    beforeEach(function() {
+      this.view.type = 'image';
+    });
+
+    describe('when rendering', function(){
       beforeEach(function() {
-         this.view.model = new Backbone.Model({
-           id: 'bef53407591f4fd8bd169f9cc02672f9',
-           type: 'text',
-           body: 'Test text asset body',
-           content: 'Test text asset body'
-         });
-         this.view.render();
+        this.view.render();
+      });
+
+      it('should provide a form for selecting an asset', function() {
+        expect(this.view.$el).toContain('form');
+      });
+
+      it('should provide a file and url input', function() {
+        expect(this.view.$el).toContain('input[name="url"]');
+        expect(this.view.$el).toContain('input[name="file"]');
+      });
+
+      it('should not include a textarea', function() {
+        expect(this.view.$el).not.toContain('textarea');
+      });
+    });
+  });
+
+  describe('with the model property set to a text asset', function() {
+    beforeEach(function() {
+       this.view.model = new Backbone.Model({
+         id: 'bef53407591f4fd8bd169f9cc02672f9',
+         type: 'text',
+         body: 'Test text asset body',
+         content: 'Test text asset body'
+       });
+       this.view.render();
+    });
+
+    describe('when rendering', function() {
+      beforeEach(function() {
+        this.view.render();
       });
 
       it("should display the model's body", function() {
