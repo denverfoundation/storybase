2846b2eb2113b7295dd261c83baedc0465e1384b
Geoffrey Hing <geoff@terrorware.com>
2012-07-17 08:41:06 -0500

    Remove unused routes

--- static/js/builder/routes.js
+++ static/js/builder/routes.js
@@ -2,23 +2,14 @@ Namespace('storybase.builder.routers');
 storybase.builder.routers.Router = Backbone.Router.extend({
   routes: {
     "story/:id": "story",
-    "selecttemplate": "selecttemplate" 
   },
 
   initialize: function(options) {
     this.dispatcher = options.dispatcher;
     this.dispatcher.on("navigate", this.navigate, this);
-    this.dispatcher.on("navigate", this.debug, this);
   },
 
   story: function(id) {
     this.dispatcher.trigger("select:story");
-  },
-
-  debug: function(fragment, options) {
-    this.navigate(fragment);
-  },
-
-  selecttemplate: function() {
   }
 });
