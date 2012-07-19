c15507abe1a8eda2b353503111c41641ffcc7b9d
Geoffrey Hing <geoff@terrorware.com>
2012-07-19 15:55:14 -0500

    Gracefully deal with the situation where the last saved date is undefined
    This happens when a story is first created.

--- static/js/builder/views.js
+++ static/js/builder/views.js
@@ -410,7 +410,7 @@ storybase.builder.views.LastSavedView = Backbone.View.extend({
 
   initialize: function() {
     console.debug(this.options.lastSaved);
-    this.lastSaved = _.isDate(this.options.lastSaved) ? this.options.lastSaved : new Date(this.options.lastSaved);
+    this.lastSaved = _.isUndefined(this.options.lastSaved) ? undefined : (_.isDate(this.options.lastSaved) ? this.options.lastSaved : new Date(this.options.lastSaved));
     this.dispatcher = this.options.dispatcher;
 
     this.dispatcher.on('save:section', this.updateLastSaved, this);
