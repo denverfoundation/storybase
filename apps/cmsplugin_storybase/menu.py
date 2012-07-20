d6b2d669dc3be86e4a35ee95af4fa4a43e40c974
Geoffrey Hing <geoff@terrorware.com>
2012-07-20 11:20:12 -0500

    Cleaned up indentation

--- apps/cmsplugin_storybase/menu.py
+++ apps/cmsplugin_storybase/menu.py
@@ -13,38 +13,37 @@ class StorybaseMenu(Menu):
     Add views from the various StoryBase apps to the CMS menu
     """
     def get_nodes(self, request): 
-	nodes = []
+        nodes = []
         explore = NavigationNode(
             title=_(plugin_settings.STORYBASE_EXPLORE_TITLE),
             url="/explore/", id='explore')
-				 
-	nodes.append(explore)
-	organizations = NavigationNode(
+        nodes.append(explore)
+        organizations = NavigationNode(
             title=_(plugin_settings.STORYBASE_ORGANIZATION_LIST_TITLE),
             url=reverse('organization_list'),
             id='organizations',
             parent_id=explore.id)
-	nodes.append(organizations)
-	for organization in Organization.objects.all().order_by('organizationtranslation__name'):
+        nodes.append(organizations)
+        for organization in Organization.objects.all().order_by('organizationtranslation__name'):
             nodes.append(
-	        NavigationNode(
-	            title=organization.name,
-		    url=organization.get_absolute_url(),
-		    id=organization.organization_id,
-		    parent_id=organizations.id))
-	projects = NavigationNode(
+            NavigationNode(
+                title=organization.name,
+                url=organization.get_absolute_url(),
+                id=organization.organization_id,
+                parent_id=organizations.id))
+        projects = NavigationNode(
             title=_(plugin_settings.STORYBASE_PROJECT_LIST_TITLE), 
             url=reverse('project_list'),
             id='projects',
             parent_id=explore.id)
-	nodes.append(projects)
-	for project in Project.objects.all().order_by('-last_edited'):
+        nodes.append(projects)
+        for project in Project.objects.all().order_by('-last_edited'):
             nodes.append(
-	        NavigationNode(
-	            title=project.name,
-		    url=project.get_absolute_url(),
-		    id=project.project_id,
-		    parent_id=projects.id))
+                NavigationNode(
+	                title=project.name,
+                    url=project.get_absolute_url(),
+                    id=project.project_id,
+                    parent_id=projects.id))
                                         
         return nodes
 
@@ -60,22 +59,22 @@ class OrderMenuNodes(Modifier):
         explore_node = None
         explore_nodes = []
         insert_at = plugin_settings.STORYBASE_EXPLORE_MENU_POSITION
-	# Check to make sure we're trying to insert the explore menu nodes
-	# in a position that actually exists
-	insert_at = insert_at if insert_at < len(nodes) else len(nodes) - 1
+        # Check to make sure we're trying to insert the explore menu nodes
+	    # in a position that actually exists
+        insert_at = insert_at if insert_at < len(nodes) else len(nodes) - 1
         for node in nodes:
            # Discover nodes in the explore menu and separate them
            if node.id == 'explore':
-                explore_node = node
-                explore_nodes.append(node)	
-	   elif explore_node in node.get_ancestors():
-	        explore_nodes.append(node)	
+               explore_node = node
+               explore_nodes.append(node)
+           elif explore_node in node.get_ancestors():
+	           explore_nodes.append(node)
            else:
-                modified_nodes.append(node)
+               modified_nodes.append(node)
 
-	# Add the explore nodes back at the specified position
+        # Add the explore nodes back at the specified position
         for node in explore_nodes:
-            modified_nodes.insert(insert_at, node) 
+            modified_nodes.insert(insert_at, node)
             insert_at += 1
         return modified_nodes
 
