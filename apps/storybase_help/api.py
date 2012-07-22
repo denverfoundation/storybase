3d56e9fc7379e0c450948ce325f4c841a5685ef6
Geoffrey Hing <geoff@terrorware.com>
2012-07-22 16:58:35 -0500

    Help API endpoint.

--- apps/storybase_help/api.py
+++ apps/storybase_help/api.py
@@ -1,12 +1,23 @@
 from django.conf.urls.defaults import url
 
 from tastypie import fields
+from tastypie import http
 from tastypie.authentication import Authentication
-from tastypie.authorization import Authorization
+from tastypie.exceptions import ImmediateHttpResponse
 from tastypie.utils import trailing_slash
+from tastypie.validation import Validation
 
-from storybase.api import TranslatedModelResource
+from storybase.api import (DelayedAuthorizationResource,
+    TranslatedModelResource, LoggedInAuthorization)
+from storybase_story.models import Section
 from storybase_help.models import Help
+
+class HelpValidation(Validation):
+    def is_valid(self, bundle, request=None):
+        errors = {} 
+        if bundle.data.get('help_id', None) is None:
+            errors['__all__'] = 'You must specify a help_id value'
+        return errors
 
 class HelpResource(TranslatedModelResource):
     # Explicitly declare fields that are on the translation model
@@ -16,11 +27,14 @@ class HelpResource(TranslatedModelResource):
     class Meta:
         queryset = Help.objects.all()
         resource_name = 'help'
-        allowed_methods = ['get']
+        list_allowed_methods = ['get', 'post']
         authentication = Authentication()
-        authorization = Authorization()
+        authorization = LoggedInAuthorization()
         # Hide the underlying id
         excludes = ['id']
+        validation = HelpValidation()
+
+        delayed_authorization_methods = []
 
     def prepend_urls(self):
         return [
@@ -32,4 +46,56 @@ class HelpResource(TranslatedModelResource):
                 (self._meta.resource_name, trailing_slash()),
                 self.wrap_view('dispatch_detail'),
                 name="api_dispatch_detail"),
+            url(r"^(?P<resource_name>%s)/sections/(?P<section_id>[0-9a-f]{32,32})%s$" %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('dispatch_list'),
+                name="api_dispatch_list"),
         ]
+
+    def apply_request_kwargs(self, obj_list, request=None, **kwargs):
+        filters = {}
+        section_id = kwargs.get('section_id')
+        if section_id:
+            filters['section__section_id'] = section_id
+
+        new_obj_list = obj_list.filter(**filters)
+
+        return new_obj_list
+
+    def is_authorized(self, request, object=None, **kwargs):
+        super(HelpResource, self).is_authorized(request, object, **kwargs)
+
+        # The call upstream didn't return an error, let's check some
+        # more resource-specific things
+        auth_result = True 
+        if request.method == 'POST':
+            section_id = kwargs.get('section_id', None)
+            if section_id:
+                # Lookup the section. It's owner should match the request's
+                section = Section.objects.get(section_id=section_id)
+                if not section.story.author == request.user:
+                    auth_result = False 
+            else:
+                # We only allow posting when a section_id is provided
+                auth_result = False
+                    
+        if not auth_result is True:
+            raise ImmediateHttpResponse(response=http.HttpUnauthorized())
+
+    def assign_help_to_section(self, bundle, request=None, **kwargs):
+        self.is_valid(bundle, request)
+        # We can assume that there will be a section id because of 
+        # this resource's implementation of is_authorized
+        section_id = kwargs.get('section_id', None)
+        section = Section.objects.get(section_id=section_id)
+        bundle.obj = self.obj_get(request, help_id=bundle.data['help_id'])
+        section.help = bundle.obj
+        section.save()
+        return bundle
+
+    def obj_create(self, bundle, request=None, **kwargs):
+        """
+        Instead of actually creating an object, delegate to another
+        hook
+        """
+        return self.assign_help_to_section(bundle, request, **kwargs)
--- apps/storybase_help/tests.py
+++ apps/storybase_help/tests.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import User
+
 from tastypie.test import ResourceTestCase, TestApiClient
 
+from storybase_story.models import create_section, create_story, Section
 from storybase_help.models import create_help
 
 class HelpResourceTest(ResourceTestCase):
@@ -8,11 +11,14 @@ class HelpResourceTest(ResourceTestCase):
         # TODO: If we end up supporting the PATCH method, use our
         # FixedTestApiClient instead of the default
         self.api_client = TestApiClient()
+        self.username = 'test'
+        self.password = 'test'
+        self.user = User.objects.create_user(self.username, 'test@example.com', self.password)
 
     def test_get_detail(self):
-        obj = create_help(title="Test Help Item",
+        help = create_help(title="Test Help Item",
                           body="Test Help Item body")
-        uri = '/api/0.1/help/%s/' % (obj.help_id)
+        uri = '/api/0.1/help/%s/' % (help.help_id)
         resp = self.api_client.get(uri)
         self.assertValidJSONResponse(resp)
         self.assertEqual(self.deserialize(resp)['title'],
@@ -21,13 +27,93 @@ class HelpResourceTest(ResourceTestCase):
                         "Test Help Item body")
 
     def test_get_detail_by_slug(self):
-        obj = create_help(title="Test Help Item",
+        help = create_help(title="Test Help Item",
                           body="Test Help Item body",
                           slug="test")
-        uri = '/api/0.1/help/%s/' % (obj.slug)
+        uri = '/api/0.1/help/%s/' % (help.slug)
         resp = self.api_client.get(uri)
         self.assertValidJSONResponse(resp)
         self.assertEqual(self.deserialize(resp)['title'],
                         "Test Help Item")
         self.assertEqual(self.deserialize(resp)['body'],
                         "Test Help Item body")
+
+    def test_get_list_for_section(self):
+        section_help = create_help(title="Test section help item",
+                          body="Test section help item body")
+        nonsection_help = create_help(title="Test non-section help item",
+                          body="Test non-section help item body",
+                          slug='test-nonsection')
+        story = create_story(title="Test Story", summary="Test Summary",
+                             byline="Test Byline", status="published",
+                             language="en")
+        section = create_section(title="Test Section 1", story=story,
+                                 help=section_help)
+
+        uri = '/api/0.1/help/sections/%s/' % (section.section_id)
+        resp = self.api_client.get(uri)
+        self.assertValidJSONResponse(resp)
+        self.assertEqual(len(self.deserialize(resp)['objects']), 1)
+        self.assertEqual(self.deserialize(resp)['objects'][0]['title'],
+                        "Test section help item")
+        self.assertEqual(self.deserialize(resp)['objects'][0]['body'],
+                        "Test section help item body")
+
+    def test_post_list_for_section(self):
+        section_help = create_help(title="Test section help item",
+                          body="Test section help item body")
+        story = create_story(title="Test Story", summary="Test Summary",
+                             byline="Test Byline", status="published",
+                             language="en", author=self.user)
+        section = create_section(title="Test Section 1", story=story)
+        self.assertEqual(section.help, None)
+        post_data = {
+            'help_id': section_help.help_id
+        }
+        uri = '/api/0.1/help/sections/%s/' % (section.section_id)
+        self.api_client.client.login(username=self.username,
+                                     password=self.password)
+        resp = self.api_client.post(uri, format='json', data=post_data)
+        self.assertHttpCreated(resp)
+        updated_section = Section.objects.get(pk=section.pk)
+        self.assertEqual(updated_section.help, section_help)
+
+    def test_post_list_for_section_unauthorized_unauthenticated(self):
+        """
+        Test that anonymous users cannot set the help item for a section
+        """
+        section_help = create_help(title="Test section help item",
+                          body="Test section help item body")
+        story = create_story(title="Test Story", summary="Test Summary",
+                             byline="Test Byline", status="published",
+                             language="en", author=self.user)
+        section = create_section(title="Test Section 1", story=story)
+        self.assertEqual(section.help, None)
+        post_data = {
+            'help_id': section_help.help_id
+        }
+        uri = '/api/0.1/help/sections/%s/' % (section.section_id)
+        resp = self.api_client.post(uri, format='json', data=post_data)
+        self.assertHttpUnauthorized(resp)
+
+    def test_post_list_for_section_unauthorized_other_user(self):
+        """
+        Test that a user can't set the help text for another user's section
+        """
+        user2 = User.objects.create(username="test2", 
+            email="test2@example.com", password="test2")
+        section_help = create_help(title="Test section help item",
+                          body="Test section help item body")
+        story = create_story(title="Test Story", summary="Test Summary",
+                             byline="Test Byline", status="published",
+                             language="en", author=user2)
+        section = create_section(title="Test Section 1", story=story)
+        self.assertEqual(section.help, None)
+        post_data = {
+            'help_id': section_help.help_id
+        }
+        self.api_client.client.login(username=self.username,
+                                     password=self.password)
+        uri = '/api/0.1/help/sections/%s/' % (section.section_id)
+        resp = self.api_client.post(uri, format='json', data=post_data)
+        self.assertHttpUnauthorized(resp)
