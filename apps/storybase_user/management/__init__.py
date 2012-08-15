5ed42d7fa42b73038ce4c1d5284751aacec87e50
Geoffrey Hing <geoff@terrorware.com>
2012-08-15 10:30:00 -0500

    Workaround for creating user profiles
    By default, a handler attached to the post_syncdb signal can
    create an initial superuser.  storybase_user also defines a post_save
    signal that creates a user profile when a user is created.
    However, when South is being used, the user profile model database tables
    haven't yet been created post_syncdb, causing an error when the superuser
    is created.  I work around this by disconnecting the handler from the
    post_syncdb signal and connecting a modified version to the post_migrate
    handler.

--- apps/storybase_user/management/__init__.py
+++ apps/storybase_user/management/__init__.py
@@ -1,6 +1,10 @@
 from django.conf import settings
+from django.contrib.auth.management import create_superuser as default_create_superuser
+from django.contrib.auth import models as auth_app
 from django.contrib.auth.models import User, Group
-from django.db.models.signals import post_syncdb
+from django.db.models.signals import post_save, post_syncdb
+
+import storybase_user as user_app
 
 # Based on django-email-usernames by Hakan Waara
 # https://bitbucket.org/hakanw/django-email-usernames
@@ -45,3 +49,38 @@ def create_admin_group(sender, app, created_models, verbosity, interactive,
                     Group.objects.create(name=settings.ADMIN_GROUP_NAME)    
                 break
 post_syncdb.connect(create_admin_group)
+
+def create_superuser(sender, app, **kwargs):
+    """
+    Custom version of the create_superuser signal handler
+    
+    This version connects to South's post_migrate signal instead of
+    post_syncd
+    """
+    from django.core.management import call_command
+
+    # Only prompt to create a user this after migrations for the user app
+    # have run and if there are really no users.
+    if (app == 'storybase_user' and kwargs.get('interactive', True) and
+            User.objects.filter(is_superuser=True).count() == 0):
+        msg = ("\nYou just installed Django's auth system, which means you "
+            "don't have any superusers defined.\nWould you like to create one "
+            "now? (yes/no): ")
+        confirm = raw_input(msg)
+        while 1:
+            if confirm not in ('yes', 'no'):
+                confirm = raw_input('Please enter either "yes" or "no": ')
+                continue
+            if confirm == 'yes':
+                call_command("createsuperuser", interactive=True)
+            break
+
+
+if 'south' in settings.INSTALLED_APPS:
+    # We have to wait until the user profile model is created before
+    # creating the initial superuser.  Otherwise, the post_save handler
+    # that creates the profile fails
+    from south.signals import post_migrate
+    post_syncdb.disconnect(default_create_superuser, sender=auth_app,
+            dispatch_uid = "django.contrib.auth.management.create_superuser")
+    post_migrate.connect(create_superuser)
