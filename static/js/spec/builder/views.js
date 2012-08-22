097f07297037226a9fce1c875c7b002a64554dd9
Geoffrey Hing <geoff@terrorware.com>
2012-08-22 15:36:00 -0500

    Removed SectionAssetEditView render test
    A lot of the heavy lifting for this view is handled by sub-views, models
    and collections.  The test, as written, would have to be more of an
    integration test than a unit test.  Maybe reimplement it later.

--- static/js/spec/builder/views.js
+++ static/js/spec/builder/views.js
@@ -36,17 +36,6 @@ describe('AppView', function() {
 describe('SectionEditView view', function() {
   beforeEach(function() {
     this.dispatcher = _.clone(Backbone.Events);
-    this.view = new storybase.builder.views.SectionEditView({
-      dispatcher: this.dispatcher,
-      model: new Backbone.Model(),
-      story: new Backbone.Model()
-    });
-  });
-
-  describe('when initializing', function() {
-    it('should have an assets property', function() {
-      expect(this.view.assets).toBeDefined();
-    });
   });
 
   describe('when initialized with an existing story and section', function () {
@@ -54,6 +43,15 @@ describe('SectionEditView view', function() {
       var Section = Backbone.Model.extend({
         url: function() {
           return '/api/0.1/stories/357c5885c4e844cb8a4cd4eebe912a1c/sections/dc044f23e93649d6b1bd48625fc301cd/'
+        }
+      });
+      var SectionAssets = Backbone.Collection.extend({
+        url: function() {
+          return '/api/0.1/stories/357c5885c4e844cb8a4cd4eebe912a1c/sections/dc044f23e93649d6b1bd48625fc301cd/assets/'
+        },
+
+        parse: function(response) {
+          return response.objects;
         }
       });
       var Story = Backbone.Model.extend({
@@ -65,34 +63,12 @@ describe('SectionEditView view', function() {
         id: "dc044f23e93649d6b1bd48625fc301cd",
         layout_template: "<div class=\"section-layout side-by-side\">\n    <div class=\"left\">\n        <div class=\"storybase-container-placeholder\" id=\"left\"></div>\n    </div>\n    <div class=\"right\">\n        <div class=\"storybase-container-placeholder\" id=\"right\"></div>\n    </div>\n</div>\n",
       }); 
+      this.section.assets = new SectionAssets;
       this.view = new storybase.builder.views.SectionEditView({
         dispatcher: this.dispatcher,
         model: this.section,
         story: new Story(), 
         templateSource: $('#section-edit-template').html()
-      });
-    });
-
-    describe('when rendering', function() {
-      beforeEach(function() {
-        this.server = sinon.fakeServer.create();
-        this.fixture = this.fixtures.SectionAssets.getList["dc044f23e93649d6b1bd48625fc301cd"];
-        this.server.respondWith(
-          "GET",
-          '/api/0.1/stories/357c5885c4e844cb8a4cd4eebe912a1c/sections/dc044f23e93649d6b1bd48625fc301cd/assets/',
-          this.validResponse(this.fixture)
-        );
-      });
-
-      afterEach(function() {
-        this.server.restore();
-      });
-
-      it('should display the assets', function() {
-        this.view.render();
-        this.server.respond();
-        expect(this.view.$el.html()).toContain(this.fixture.objects[0].asset.content);
-        expect(this.view.$el.html()).toContain(this.fixture.objects[1].asset.content);
       });
     });
 
