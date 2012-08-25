2cfb7608f3bb3001e0f6a8d7a807219f2f483eb8
Eric Miller <github@patternleaf.com>
2012-08-25 11:17:23 -0600

    homepage work - plugins for intro/header and special content - template tags for featured projects and featured stories + mock content
    unsure if the template tags and their dependencies all went into the right spots.

--- apps/storybase_story/templatetags/story.py
+++ apps/storybase_story/templatetags/story.py
@@ -1,4 +1,5 @@
 from django import template
+from storybase_story.views import (render_featured_projects, render_featured_stories)
 
 register = template.Library()
 
@@ -8,3 +9,12 @@ def container(context, name):
         return context['asset_content'][name]
     except KeyError:
         return '<div class="storybase-container-placeholder" id="%s"></div>' % (name)
+
+@register.simple_tag
+def featured_stories(count = 4):
+    return render_featured_stories(count);
+
+# should this go in _user?
+@register.simple_tag
+def featured_projects(count = 4):
+    return render_featured_projects(count);
--- apps/storybase_story/views.py
+++ apps/storybase_story/views.py
@@ -1,6 +1,7 @@
 """Views for storybase_story app"""
 
 import json
+import random # temp
 
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
@@ -49,6 +50,46 @@ def homepage_banner_list(num_stories):
             image_num -= 9
         stories.append({"image": "image%d.jpg" % image_num, "title": "banner story %d title here." % i})
     return stories
+    
+lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+def render_featured_projects(count = 4):
+    # temp: should actually pull projects, etc.    
+    # currently the template asks for a "normalized" dictionary format, so 
+    # note that passing raw project objects won't work.
+
+    #projects = Project.objects.on_homepage().order_by('-last_edited')[:count]
+    projects = []
+    for i in range(count):
+        projects.append({ 
+            "title": "Project %d Title" % (i + 1),
+            "author": "Author Name", 
+            "date": "August 25, 2012", 
+            "image_url": "/static/css/images/image%d.jpg" % random.randrange(1, 9), 
+            "excerpt": lorem 
+        })
+
+    template = get_template('featured_objects.html')
+    context = Context({ "objects": projects, "more_link_text": "View Projects", "more_link_url": "/projects"})
+    return template.render(context)
+
+def render_featured_stories(count = 4):
+    # temp: should actually pull projects, etc.
+    # currently the template asks for a "normalized" dictionary format, so 
+    # note that passing raw project objects may not work.
+    stories = []
+    for i in range(count):
+        stories.append({ 
+            "title": "Story %d Title" % (i + 1),
+            "author": "Author Name", 
+            "date": "August 25, 2012", 
+            "image_url": "/static/css/images/image%d.jpg" % random.randrange(1, 9), 
+            "excerpt": lorem 
+        })
+
+    template = get_template('featured_objects.html')
+    context = Context({ "objects": stories, "more_link_text": "View Stories", "more_link_url": "/stories"})
+    return template.render(context)
 
 
 class ExploreStoriesView(TemplateView):
