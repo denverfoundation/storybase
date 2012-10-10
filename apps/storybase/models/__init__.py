fd7319a21bb74e182cfbb8a3508db33c974b33ae
Geoff Hing <geoffhing@gmail.com>
2012-10-10 12:47:56 -0500

    A timezone-aware version of DirtyFieldsMixin
    This was needed when experimenting with mixing DirtyFields into the
    Story class.  I think the API gets timezone-aware datetimes and
    DirtyFieldsMixin.get_dirty_fields was failing when trying to compare
    timezone-aware datetimes with timezone-naive ones.  I modified
    DirtyFieldsMixin to use an overridable comparison function that is
    overrided in TzDirtyFieldsMixin to handle this case.

--- apps/storybase/models/__init__.py
+++ apps/storybase/models/__init__.py
@@ -1,6 +1,6 @@
 from storybase.models.base import (LicensedModel, PublishedModel,
                                    TimestampedModel, set_date_on_published)
-from storybase.models.dirtyfields import DirtyFieldsMixin
+from storybase.models.dirtyfields import DirtyFieldsMixin, TzDirtyFieldsMixin
 from storybase.models.permission import PermissionMixin
 from storybase.models.translation import TranslatedModel, TranslationModel
 
---
+++ apps/storybase/utils/timezone.py
@@ -0,0 +1,93 @@
+"""Backport of selected helper functions from django.utils.timezone"""
+
+import pytz
+
+from django.conf import settings
+
+# Copyright (c) Django Software Foundation and individual contributors.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#    
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#    3. Neither the name of Django nor the names of its contributors may be used
+#       to endorse or promote products derived from this software without
+#       specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# In order to avoid accessing the settings at compile time,
+# wrap the expression in a function and cache the result.
+_localtime = None
+
+def get_default_timezone():
+    """
+    Returns the default time zone as a tzinfo instance.
+
+    This is the time zone defined by settings.TIME_ZONE.
+
+    See also :func:`get_current_timezone`.
+    """
+    global _localtime
+    if _localtime is None:
+        if isinstance(settings.TIME_ZONE, str):
+            _localtime = pytz.timezone(settings.TIME_ZONE)
+    return _localtime
+
+# By design, these four functions don't perform any checks on their arguments.
+# The caller should ensure that they don't receive an invalid value like None.
+
+def is_aware(value):
+    """
+    Determines if a given datetime.datetime is aware.
+
+    The logic is described in Python's docs:
+    http://docs.python.org/library/datetime.html#datetime.tzinfo
+    """
+    return value.tzinfo is not None and value.tzinfo.utcoffset(value) is not None
+
+def is_naive(value):
+    """
+    Determines if a given datetime.datetime is naive.
+
+    The logic is described in Python's docs:
+    http://docs.python.org/library/datetime.html#datetime.tzinfo
+    """
+    return value.tzinfo is None or value.tzinfo.utcoffset(value) is None
+
+def make_aware(value, timezone):
+    """
+    Makes a naive datetime.datetime in a given time zone aware.
+    """
+    if hasattr(timezone, 'localize'):
+        # available for pytz time zones
+        return timezone.localize(value, is_dst=None)
+    else:
+        # may be wrong around DST changes
+        return value.replace(tzinfo=timezone)
+
+def make_naive(value, timezone):
+    """
+    Makes an aware datetime.datetime naive in a given time zone.
+    """
+    value = value.astimezone(timezone)
+    if hasattr(timezone, 'normalize'):
+        # available for pytz time zones
+        value = timezone.normalize(value)
+    return value.replace(tzinfo=None)
