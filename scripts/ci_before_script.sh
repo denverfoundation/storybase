23998a353264f5fb397aef146cbe0bbc47c3b00b
Geoff Hing <geoffhing@gmail.com>
2012-10-11 14:17:52 -0500

    Break Travis CI setup steps out into shell script

---
+++ scripts/ci_before_script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh  
+
+sudo bash ./scripts/apply_django_patches /home/vagrant/virtualenv/python2.7/lib/python2.7/site-packages/django
+fab --set run_local=True install_solr upgrade_to_solr3 install_solr_2155
+fab --set run_local=True install_solr_config:instance=travis,solr_multicore=,project_root=`pwd`
+fab --set run_local=True enable_jetty_start
+createdb -T template_postgis atlas_travis -U postgres
+python manage.py build_solr_schema --settings=settings.travis > config/travis/solr/schema.xml
+sudo cp config/travis/solr/schema.xml /usr/local/share/solr3/conf/
+fab --set run_local=True update_solr_jetty_config
+sudo service jetty restart
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+
