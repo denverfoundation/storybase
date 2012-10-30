be943610b1eb0b215d6752d7b77ce3ad53776669
Geoff Hing <geoffhing@gmail.com>
2012-10-29 22:29:34 -0500

    Make contact for a Django CMS plugin
    This allows the user to configure the content on the contact page.
    
    Implementation for #306

--- apps/cmsplugin_storybase/cms_plugins.py
+++ apps/cmsplugin_storybase/cms_plugins.py
@@ -1,13 +1,25 @@
 from django.utils.translation import ugettext as _
 
 from cms.plugin_base import CMSPluginBase
+from cms.models.pluginmodel import CMSPlugin
 from cms.plugin_pool import plugin_pool
 
 from storybase_story.views import (homepage_story_list, homepage_banner_list)
 from storybase_user.views import (homepage_organization_list,
                                   homepage_project_list)
+from storybase_messaging.forms import SiteContactMessageForm 
 
 from cmsplugin_storybase.models import List, StoryPlugin as StoryPluginModel
+
+
+class ContactFormPlugin(CMSPluginBase):
+    model = CMSPlugin
+    name = _("StoryBase Contact Form")
+    render_template = "sitecontactmessage_form_plugin.html"
+
+    def render(self, context, instance, placeholder):
+        context['form'] = SiteContactMessageForm()
+        return context
 
 class StoryPlugin(CMSPluginBase):
     model = StoryPluginModel
@@ -55,6 +67,7 @@ class HomepageBannerPlugin(CMSPluginBase):
 	return context
 
 
+plugin_pool.register_plugin(ContactFormPlugin)
 plugin_pool.register_plugin(StoryPlugin)
 plugin_pool.register_plugin(HomepageStoriesPlugin)
 plugin_pool.register_plugin(HomepageBannerPlugin)
--- apps/storybase_messaging/views.py
+++ apps/storybase_messaging/views.py
@@ -18,24 +18,30 @@ class SiteContactMessageCreateView(CreateView):
         Determine the URL to redirect to when the form is successfully 
         validated
         """
-        # Just redirect back to the contact page for now
-        return reverse('contact')
+        success_url = self.request.GET.get('success', None)
+        # See if a success URL was passed as part of the querystring
+        if success_url:
+            return success_url
+        else:
+            # Just redirect back to the contact page for now
+            return reverse('contact')
    
     def form_valid(self, form):
-	"""
-	Save the form instance, set the current object for the view and 
-	redirect to get_success_url()
+        """
+        Save the form instance, set the current object for the view and 
+        redirect to get_success_url()
 
-	Flashes a success message to the user
-	"""
+        Flashes a success message to the user
+        """
         messages.add_message(self.request, messages.SUCCESS, 
-	                     _("Thanks! Your message has been submitted to the "
-			       "site administrators"),
-			     fail_silently=False)
-	return super(SiteContactMessageCreateView, self).form_valid(form)
+                         _("Thanks! Your message has been submitted to the "
+                   "site administrators"),
+                 fail_silently=False)
+
+        return super(SiteContactMessageCreateView, self).form_valid(form)
 
     def form_invalid(self, form):
-	messages.add_message(self.request, messages.ERROR,
-			     _("Please fill out all required information"),
-			     fail_silently=False)
-	return super(SiteContactMessageCreateView, self).form_invalid(form)
+        messages.add_message(self.request, messages.ERROR,
+                 _("Please fill out all required information"),
+                 fail_silently=False)
+        return super(SiteContactMessageCreateView, self).form_invalid(form)
