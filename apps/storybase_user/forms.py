de66176214c1a9db29442bbf70ab9f26048bc989
Geoff Hing <geoffhing@gmail.com>
2012-12-06 08:29:00 -0600

    Clean up spacing/indentation

--- apps/storybase_user/forms.py
+++ apps/storybase_user/forms.py
@@ -5,4 +5,5 @@ from storybase_user.models import UserProfile
 class UserNotificationsForm(ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('notify_updates', 'notify_admin', 'notify_digest', 'notify_story_featured', 'notify_story_comment')
+        fields = ('notify_updates', 'notify_admin', 'notify_digest',
+                  'notify_story_featured', 'notify_story_comment')
