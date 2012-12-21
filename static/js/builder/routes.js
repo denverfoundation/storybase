a68c29039c0c12c9c6e3b81a88c10414561cfa25
Geoff Hing <geoffhing@gmail.com>
2012-12-21 16:40:44 -0600

    Reload builder when story ID is in a hash
    In IE9 (and presumably other browsers that don't support the History
    API) Backbone falls back to using hashes to route to different views in
    the application.  This however, causes a problem when reloading pages
    because, without the ID in the URL, Django doesn't know to include the
    JSON to bootstrap the Backbone views.
    
    In the router, we detect this case, and force a reload of the page in
    the browser (with the hash removed) to properly bootstrap the views.
    
    Addresses #417

--- static/js/builder/routes.js
+++ static/js/builder/routes.js
@@ -1,4 +1,6 @@
 Namespace('storybase.builder.routers');
+Namespace.use('storybase.utils.hasAnalytics');
+
 storybase.builder.routers.Router = Backbone.Router.extend({
   initialize: function(options) {
     this.dispatcher = options.dispatcher;
@@ -40,9 +42,19 @@ storybase.builder.routers.Router = Backbone.Router.extend({
       // The page was loaded with an id via a hash, so the views haven't
       // been bootstrapped.  Force a page refresh so that the views will
       // be properly bootstrapped.
-      // BOOKMARK
-      // TODO: Check how to generate the url to refresh in IE
-      url = window.location.href;
+      if (hasAnalytics()) {
+        _gaq.push(['_trackEvent', 'Routes', 'Page load with hashed story ID']);
+      }
+      if (window.location.hash) {
+        // Remove the hash from the URL. The server will generate the 
+        // correct content for the workflow step when requested directly
+        url = window.location.href.replace('#', '');
+      }
+      else {
+        // No hash, likely because the browser supports the History API and
+        // Backbone has already updated the location to be hash-less
+        url = window.location.href;
+      }
       window.location.replace(url);
     }
     else {
