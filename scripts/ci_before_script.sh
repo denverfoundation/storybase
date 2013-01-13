46d1a08af62e1eb706f0f09ff3e554355f0b0a0f
Geoff Hing <geoffhing@gmail.com>
2013-01-13 10:14:27 -0600

    Tell Jetty about the JDK
    Addresses #564

--- scripts/ci_before_script.sh
+++ scripts/ci_before_script.sh
@@ -8,8 +8,9 @@ createdb -T template_postgis atlas_travis -U postgres
 python manage.py build_solr_schema --settings=settings.travis > config/travis/solr/schema.xml
 sudo cp config/travis/solr/schema.xml /usr/local/share/solr3/conf/
 # BEGIN DEBUG
-cat /etc/default/jetty
-dpkg --list | grep -i jdk
+ls /usr/lib/jvm/default-java
+sudo sed -i.bak -r -e "s/#JDK_DIRS/JDK_DIRS/g" /etc/default/jetty
+grep JDK_DIRS /etc/default/jetty
 # END DEBUG
 fab --set run_local=True update_solr_jetty_config
 sudo service jetty restart
