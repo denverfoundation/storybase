ed45e4841731ce66cf5fed76fa847b2cdc2e5fe7
Geoff Hing <geoffhing@gmail.com>
2013-03-25 09:01:46 -0500

    Fix failing test
    So this wasn't a race condition, just some silliness on my part. The story
    saves were happening in within less than one second of each other, so when
    they were converted to an epoch by get_weight and converted to an integer
    for storing in the weight field (an IntegerField), the weight values are
    the same. If the comparisons were <= instead of <, the tests would pass,
    but since the comparisons used <, they were failing.
    
    I don't think having duplicate weight values for closely published stories
    is a problem at all, but I feel like this test should reflect the case
    where stories are written in a specific order and have distinctly different
    weights.  I updated the call to sleep to ensure that the saves occur at
    least one second apart.
    
    Addresses #625

--- apps/storybase_story/tests.py
+++ apps/storybase_story/tests.py
@@ -444,21 +444,18 @@ class StoryModelTest(TestCase, SloppyComparisonTestMixin):
                 byline="Test byline", status="draft")
         StoryRelation.objects.create(source=seed_story, target=connected_story,
                 relation_type='connected')
-        # Wait to ensure that seed story database write has
-        # finished
-        # TODO: Figure out why there's this race condition. I couldn't get
-        # it to fail consistently
-        sleep(0.25)
+        # Wait to ensure that the published timestamps of the two stories
+        # differ by >= 1 second
+        sleep(1)
         # Create another story.  This should have a greater
         # sort weight than the seed story because it was
         # published later
         story = create_story(title="Test Story", summary="Test Summary",
             byline="Test Byline", status='published')
         self.assertTrue(story.weight > seed_story.weight)
-        # Wait to ensure that the story database write has completed 
-        # TODO: Figure out why there's this race condition. I couldn't get
-        # it to fail consistently
-        sleep(0.25)
+        # Wait to ensure that the published timestamps of the two stories
+        # differ by >= 1 second
+        sleep(1)
         # Publish the connected story.  This should bump
         # the sort weight of the seed story 
         connected_story.status = 'published'
