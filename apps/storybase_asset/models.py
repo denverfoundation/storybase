1d47fe5ad539cc1cd0256416931fd0c6d2d8d24a
Geoff Hing <geoffhing@gmail.com>
2013-06-03 11:09:52 -0500

    Remove unused import

--- apps/storybase_asset/models.py
+++ apps/storybase_asset/models.py
@@ -6,7 +6,6 @@ import lxml.html
 
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.contrib.sites.models import Site
 from django.core.files import File
 from django.core.cache import cache
 from django.db import models
