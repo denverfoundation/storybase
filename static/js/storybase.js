579ae7182529ea41616eef7f23aaf28d499f1dec
Geoff Hing <geoffhing@gmail.com>
2013-06-14 15:22:35 -0500

    Try to open external links in new tab
    Specifically for Firefox, try to open external links in a new
    tab (the default) instead of a new window. It seems that Firefox's
    implementation of ``window.open`` will always try to open in a new
    window of the third argument (window options) is passed, even if
    it's undefined.
    
    To get around this, check if the window options variable evaluates
    to truthy and only call ``window.open`` with the third argument if
    the options are actually provided.
    
    Addresses #583

--- static/js/storybase.js
+++ static/js/storybase.js
@@ -8,16 +8,25 @@
   }
   var storybase = this.storybase;
 
+  // TODO: Use this instead of similar handlers elsewhere in the code
   /**
    * Click handler that opens a link in a new window
    */
   storybase.openInNewWindow = function(evt) {
-    // TODO: Use this instead of similar handlers elsewhere in the code
     evt.preventDefault();
     var url = $(evt.currentTarget).attr('href'); 
     var windowName = $(evt.target).data('window-name') || '_blank';
     var windowFeatures = $(evt.target).data('window-features');
-    window.open(url, windowName, windowFeatures);
+   
+    // In Firefox, if windowFeatures is passed, the new window will be
+    // in a window rather than a tab. Generally, we want to open things
+    // in a new tab, so only pass the third argument if it's truthy
+    if (windowFeatures) {
+      window.open(url, windowName, windowFeatures);
+    }
+    else {
+      window.open(url, windowName);
+    }
     return false;
   };
 }).call(this, jQuery);
