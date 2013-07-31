cce0a83102b17fd43d4da8de44faff88c1d0ab6a
Geoff Hing <geoffhing@gmail.com>
2013-07-31 10:40:22 -0500

    More robust error handling/reporting
    Use ``storybase.messaging.management`` for the logger to allow
    for more fine-grained handling of these logs (since the management
    command is meant to run frequently from cron, the logs can get
    pretty verbose).
    
    Catch all exceptions and log errors so the command doesn't die
    inexplicably.
    
    Addresses #580

--- apps/storybase_messaging/management/commands/send_story_notifications.py
+++ apps/storybase_messaging/management/commands/send_story_notifications.py
@@ -4,7 +4,7 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand
 
-logger = logging.getLogger('storybase.management')
+logger = logging.getLogger('storybase.messaging.management')
 
 class Command(BaseCommand):
     help = ("Send story notification emails whose send on date/time is now\n"
@@ -20,19 +20,24 @@ class Command(BaseCommand):
             )
     
     def handle(self, *args, **options):
-        send_on = options.get('send_on', None)
-        if send_on is not None:
-            # If specified, convert the send_on option from a string
-            # to a datetime object
-            send_on = datetime.strptime(send_on)
+        try:
+            send_on = options.get('send_on', None)
+            if send_on is not None:
+                # If specified, convert the send_on option from a string
+                # to a datetime object
+                send_on = datetime.strptime(send_on)
 
-        from storybase_messaging.models import StoryNotification
-        (sent, unsent) = StoryNotification.objects.send_emails(send_on=send_on)
+            from storybase_messaging.models import StoryNotification
+            (sent, unsent) = StoryNotification.objects.send_emails(send_on=send_on)
 
-        message = "%d story notifications sent" % (len(sent))
-        if len(unsent):
-            message += " %d story notifications failed" % (len(unsent))
+            message = "%d story notifications sent" % (len(sent))
+            if len(unsent):
+                message += " %d story notifications failed" % (len(unsent))
 
-        logger.info(message)
-        if int(options.get('verbosity')) > 1:
-            self.stdout.write(message + "\n")
+            logger.info(message)
+            if int(options.get('verbosity')) > 1:
+                self.stdout.write(message + "\n")
+        except Exception, e:
+            message = "Uncaught exception when sending story notifications: %s" % (e)
+            logger.error(message)
+            self.stdout.write(message)
