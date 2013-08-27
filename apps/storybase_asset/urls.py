e904ec2e40bbf11a3a466c83a128f30056607267
Geoff Hing <geoffhing@gmail.com>
2013-08-27 13:17:44 -0500

    storybase_asset.urls regexs should begin with '^'
    If the regexs in storybase_asset.urls don't begin with '^',
    they match paths like /api/0.1/assets/ASSET_ID/ when they should
    only match /assets/ASSET_ID/

--- apps/storybase_asset/urls.py
+++ apps/storybase_asset/urls.py
@@ -5,11 +5,11 @@ from django.conf.urls import patterns, url
 from storybase_asset.views import AssetDetailView, DataSetDetailView, AssetContentView
 
 urlpatterns = patterns('',
-    url(r'assets/(?P<asset_id>[0-9a-f]{32,32})/content$',
+    url(r'^assets/(?P<asset_id>[0-9a-f]{32,32})/content$',
 	AssetContentView.as_view(), name='asset_content_view'), 
-    url(r'assets/(?P<asset_id>[0-9a-f]{32,32})/$', AssetDetailView.as_view(),
+    url(r'^assets/(?P<asset_id>[0-9a-f]{32,32})/$', AssetDetailView.as_view(),
 	name='asset_detail'), 
-    url(r'datasets/(?P<dataset_id>[0-9a-f]{32,32})/$',
+    url(r'^datasets/(?P<dataset_id>[0-9a-f]{32,32})/$',
 	DataSetDetailView.as_view(), name='dataset_detail'),
   
 )
