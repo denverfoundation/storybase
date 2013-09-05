denverfoundation/storybase

commit 05c54cc587f113b37ed188c77d55a68297fe22cf

Update notification types
Update the types of notifications that a user can receive. This requires
updating the user profile model to store the notification settings and
updating the microcopy for the notification settings form.

Addresses #811

apps/storybase_user/admin.py
@@ -53,7 +53,7 @@ class UserProfileInline(admin.StackedInline):
         (
              _('Notifications'), 
              {
-                 'fields': ('notify_updates', 'notify_admin', 'notify_digest', 'notify_story_featured', 'notify_story_comment'),
+                 'fields': ('notify_admin', 'notify_digest', 'notify_story_unpublished', 'notify_story_published', 'notify_story_comment'),
                  'description': _("Users will receive emails about these kind of events"),
              }
         ),

apps/storybase_user/forms.py
@@ -62,5 +62,6 @@ class ProjectModelForm(StoryAggregatorModelFormBase):
 class UserNotificationsForm(ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('notify_updates', 'notify_admin', 'notify_digest',
-                  'notify_story_featured', 'notify_story_comment')
+        fields = ('notify_admin', 'notify_digest',
+                  'notify_story_unpublished', 'notify_story_published',
+                  'notify_story_comment')

apps/storybase_user/models.py
@@ -464,20 +464,21 @@ class UserProfile(RecentStoriesMixin, models.Model):
     # Notification preferences
     # Right now these represent e-mail contact
     # Floodlight updates
-    notify_updates = models.BooleanField("Floodlight Updates", default=True,
-        help_text="Updates about new Floodlight features, events and storytelling tips")
     notify_admin = models.BooleanField("Administrative Updates",
         default=True,
         help_text="Administrative account updates")
-    notify_digest = models.BooleanField("Monthly Digest", default=True,
-        help_text="A monthly digest of featured Floodlight stories")
+    notify_digest = models.BooleanField("Weekly Digest", default=True,
+        help_text="A weekly digest of Floodlight stories, events and new features")
     # Notifications about my stories
-    notify_story_featured = models.BooleanField("Homepage Notification",
+    notify_story_unpublished = models.BooleanField("Unpublished Story Reminder",
         default=True,
-        help_text="One of my stories is featured on the Floodlight homepage or newsletter")
+        help_text="Remind me to complete unpublished stories")
+    notify_story_published = models.BooleanField("Published Story Notification",
+        default=True,
+        help_text="Alert me when I publish a story")
     notify_story_comment = models.BooleanField("Comment Notification",
         default=True,
-        help_text="Someone comments on one of my stories")
+        help_text="Alert me when someone comments on one of my stories")
 
     def __unicode__(self):
         return unicode(self.user)

apps/storybase_user/templates/storybase_user/account_notifications.html
@@ -15,28 +15,28 @@
         <legend>{{ storybase_site_name }} {% trans "updates" %}</legend>
         <p>{% trans "Email me with" %}</p>
         <div class="form-field-container">
-        {{ form.notify_updates }}
-        <label for="{{ form.notify_updates.html_name }}">{% trans "Updates about new Floodlight features, events and storytelling tips" %}</label>
-        </div>
-        <div class="form-field-container">
             {{ form.notify_admin }}
             <label for="{{ form.notify_admin.html_name }}">{% trans "Administrative account updates" %}
         </div>
         <div class="form-field-container">
             {{ form.notify_digest }}
-            <label for="{{ form.notify_digest.html_name }}">{% trans "A monthly digest of featured Floodlight stories" %}
+            <label for="{{ form.notify_digest.html_name }}">{% trans "A weekly digest of Floodlight stories, events and new features" %}
         </div>
     </fieldset>
     <fieldset>
         <legend>{% trans "Notifications about my stories" %}</legend>
-        <p>{% trans "Email me when" %}</p>
+        <p>{% trans "Email me to" %}</p>
         <div class="form-field-container">
-            {{ form.notify_story_featured }}
-            <label for="{{ form.notify_story_featured.html_name }}">{% trans "One of my stories is featured on the Floodlight homepage or newsletter" %}</label>
+            {{ form.notify_story_unpublished }}
+            <label for="{{ form.notify_story_unpublished.html_name }}">{% trans "Remind me to complete unpublished stories" %}</label>
+        </div>
+        <div class="form-field-container">
+            {{ form.notify_story_published }}
+            <label for="{{ form.notify_story_published.html_name }}">{% trans "Alert me when I publish a story" %}</label>
         </div>
         <div class="form-field-container">
             {{ form.notify_story_comment }}
-            <label for="{{ form.notify_story_comment.html_name }}">{% trans "Someone comments on one of my stories" %}</label>
+            <label for="{{ form.notify_story_comment.html_name }}">{% trans "Alert me when someone comments on one of my stories" %}</label>
         </div>
     </fieldset>
     <input type="submit" value="{% trans "Save" %}" />

commit 138e037e6f8a71c9085065bb0328a7adf513da0c

Reference correct profile fields in CSV export
Reference the proper profile fields for user notification settings
that were changed as requested in #811.

This is a regression introducted by
05c54cc587f113b37ed188c77d55a68297fe22cf.

Closes #859

apps/storybase_user/admin.py
@@ -172,7 +172,9 @@ class StoryUserAdmin(UserAdmin):
         response['Content-Disposition'] = 'attachment; filename=%s' % (filename)
         user_field_names = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined', 'last_login',)
         # TODO: See if I can use methods of the modeladmin as fields
-        profile_field_names = ('notify_updates', 'notify_admin', 'notify_digest', 'notify_story_featured', 'notify_story_comment')
+        profile_field_names = ('notify_admin', 'notify_digest',
+            'notify_story_unpublished', 'notify_story_published',
+            'notify_story_comment')
         writer = csv.writer(response)
         writer.writerow(header_row(user_field_names, profile_field_names))
         for obj in queryset: