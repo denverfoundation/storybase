27589b2bb59f821106c3c6eb61d8c8c7393b13c0
Geoff Hing <geoffhing@gmail.com>
2013-09-09 18:23:54 +0000

    Update Solr install fabric tasks
    Update the fabric tasks that install Solr/Jetty to use the Solr
    example.
    
    Install the Solr-2155 plugin to the core's lib directory instead
    of globally.
    
    Update the Travis build configuration to use this Solr install.

--- scripts/ci_before_install.sh
+++ scripts/ci_before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get install gdal-bin libproj-dev postgis postgresql-9.1-postgis
-sudo sed -i.bak -r -e "s/^#JAVA_HOME=/JAVA_HOME=\/usr\/lib\/jvm\/java-7-oracle/g" /etc/default/jetty
+#sudo sed -i.bak -r -e "s/^#JAVA_HOME=/JAVA_HOME=\/usr\/lib\/jvm\/java-7-oracle/g" /etc/default/jetty
 sudo bash ./scripts/create_template_postgis-debian.sh
 
