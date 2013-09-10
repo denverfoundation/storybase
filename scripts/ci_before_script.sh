6ca2189cd4ad6e52a215f5773501540b5f79a305
Geoff Hing <geoffhing@gmail.com>
2013-09-10 04:31:42 +0000

    Make Solr core directories as part of Travis install

--- scripts/ci_before_script.sh
+++ scripts/ci_before_script.sh
@@ -2,7 +2,7 @@
 
 sudo ./scripts/apply_django_patches /home/vagrant/virtualenv/python2.7/lib/python2.7/site-packages/django
 createdb -T template_postgis atlas_travis -U postgres
-fab --set run_local=True install_solr install_solr_2155
+fab --set run_local=True install_solr make_solr_conf_dir make_solr_data_dir make_solr_lib_dir install_solr_2155
 fab --set run_local=True install_jetty_script install_jetty_config
 sudo cp config/travis/solr/solr.xml /usr/local/share/solr/multicore/
 python manage.py build_solr_schema --settings=settings.travis > config/travis/solr/schema.xml
