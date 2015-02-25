2ad8e2d4a31f191c03a25f7089c46afda7d67281
Wilberto Morales <wilbertomorales777@gmail.com>
2015-02-25 08:05:07 -0600

    Patching from the badge api

--- apps/storybase_badge/api.py
+++ apps/storybase_badge/api.py
@@ -1,10 +1,18 @@
 
+from tastypie.authorization import Authorization
+from tastypie.authentication import Authentication
 from tastypie.resources import ModelResource
+from tastypie import fields
+
 from storybase_badge.models import Badge
 
 
 class BadgeResource(ModelResource):
 
+    stories = fields.ToManyField('storybase_story.api.StoryResource', 'stories', full=False)
+
     class Meta:
+        authorization = Authorization()
+        authentication = Authentication()
         queryset = Badge.objects.all()
         resource_name = 'badges'
--- apps/storybase_badge/tests.py
+++ apps/storybase_badge/tests.py
@@ -1,10 +1,20 @@
 
 from tastypie.test import ResourceTestCase
+from storybase_story.models import create_story
+from storybase_badge.models import Badge
 
 
 class BadgeResourceTest(ResourceTestCase):
 
     fixtures = ['denver_badges.json']
+
+    def setUp(self):
+
+        super(BadgeResourceTest, self).setUp()
+
+        self.story = create_story(
+            'Nature Remix'
+        )
 
     def test_get_list_of_badges(self):
 
@@ -18,5 +28,32 @@ class BadgeResourceTest(ResourceTestCase):
             u'icon_uri': '/static/img/badges/denver-foundation.png',
             u'id': 1,
             u'name': u'Denver Foundation',
-            u'resource_uri': '/api/0.1/badges/1/'
+            u'resource_uri': '/api/0.1/badges/1/',
+            u'stories': []
         })
+
+    def test_add_badge_to_story(self):
+
+        badge = Badge.objects.all()[0]
+
+        self.assertNotIn(badge, self.story.badges.all())
+
+        self.api_client.patch('/api/0.1/badges/{}/'.format(badge.id), data={
+            'stories': ['/api/0.1/stories/{}/'.format(self.story.story_id)]
+        })
+
+        self.assertIn(badge, self.story.badges.all())
+
+    def test_remove_badge_from_story(self):
+
+        badge = Badge.objects.all()[0]
+
+        self.assertNotIn(self.story, badge.stories.all())
+        badge.stories.add(self.story)
+        self.assertIn(self.story, badge.stories.all())
+
+        self.api_client.patch('/api/0.1/badges/{}/'.format(badge.id), data={
+            'stories': []
+        })
+
+        self.assertNotIn(self.story, badge.stories.all())
