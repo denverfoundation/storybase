3c7fc5d5058da15fe3e26be219c7a73996ac4bd2
Wilberto Morales <wilbertomorales777@gmail.com>
2015-03-02 20:14:59 -0600

    Only allow get and patch on bages resource and BadgeAuthorization

--- apps/storybase_badge/api.py
+++ apps/storybase_badge/api.py
@@ -1,10 +1,21 @@
 
-from tastypie.authorization import Authorization
+from tastypie.authorization import ReadOnlyAuthorization, Unauthorized
 from tastypie.authentication import Authentication
 from tastypie.resources import ModelResource
 from tastypie import fields
 
+from storybase.api.authorization import UserAuthorization
 from storybase_badge.models import Badge
+
+
+class BadgeAuthorization(ReadOnlyAuthorization, UserAuthorization):
+
+    def update_detail(self, object_list, bundle):
+
+        if self.user_valid(bundle) and bundle.request.user.get_profile().can_edit_badge(bundle.obj):
+            return True
+        else:
+            raise Unauthorized()
 
 
 class BadgeResource(ModelResource):
@@ -12,7 +23,9 @@ class BadgeResource(ModelResource):
     stories = fields.ToManyField('storybase_story.api.StoryResource', 'stories', full=False)
 
     class Meta:
-        authorization = Authorization()
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get', 'patch']
         authentication = Authentication()
         queryset = Badge.objects.all()
         resource_name = 'badges'
+
