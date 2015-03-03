2d91823f64ceacc3a5169c6f53a49861189549b3
Wilberto Morales <wilbertomorales777@gmail.com>
2015-03-02 23:07:27 -0600

    Basic code for adding badges dynamically

--- static/js/badges/models.js
+++ static/js/badges/models.js
@@ -9,9 +9,37 @@
   }
 
   var Badge = Backbone.Model.extend({
-    
+
   });
 
+  var Badges = Backbone.Collection.extend(
+    _.extend({}, storybase.collections.TastypieMixin, {
+      url: function() {
+        return storybase.API_ROOT + 'badges/';
+      },
+      model: Badge
+    })
+  );
+
+  var StoryBadges = function(story) {
+    this.story = story;
+    this.story_id = this.story.id;
+    this.storyUri = storybase.API_ROOT + 'stories/' + this.story_id + '/';
+  }
+
+  StoryBadges.prototype.addBadge = function(badge) {
+    var stories = badge.get('stories');
+    stories.push(this.storyUri);
+
+    badge.save({stories: stories}, {patch: true});
+
+    return this;
+  }
+
+  var Story = storybase.models.Story;
+
   storybase.badges.models.Badge = Badge;
+  storybase.badges.collections.Badges = Badges;
+  storybase.badges.models.StoryBadges = StoryBadges;
 
 })(_, Backbone, storybase);
