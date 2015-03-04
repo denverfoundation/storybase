0895591a339c486794be31f9c366987507129b39
Wilberto Morales <wilbertomorales777@gmail.com>
2015-03-04 10:35:34 -0600

    Views for removing and adding badges

--- static/js/badges/views.js
+++ static/js/badges/views.js
@@ -14,12 +14,13 @@
   });
 
   var BadgesView = Backbone.View.extend({
+    SubView: BadgeView,
     render: function() {
-
       this.$el.html('');
 
       this.collection.each(function(badge){
-        var bv = new BadgeView({model: badge});
+        var bv = new this.SubView({model: badge});
+        bv.parent = this;
         this.$el.append(bv.render().el);
 
       }, this);
@@ -29,7 +30,50 @@
 
   });
 
+  var ClickableBadgeView = BadgeView.extend({
+    events: {
+      'dblclick .badge': 'clicked'
+    },
+    clicked: function(event) {
+      this.parent.clickedSubView(event, this);
+    }
+  });
+
+  var StoryBadgesEditorView = BadgesView.extend({
+    initialize: function(options) {
+      this.userBadges = options.userBadges;
+      this.storyBadges = options.storyBadges;
+      this.badgesView = options.badgesView;
+
+    },
+    clickedSubView: function(event, view) {
+      this.badgesView.render();
+      view.remove();
+    },
+    SubView: ClickableBadgeView,
+  });
+
+  var RemoveBadgesView = StoryBadgesEditorView.extend({
+    clickedSubView: function(event, view) {
+      this.storyBadges.removeBadge(view.model);
+      StoryBadgesEditorView.prototype.clickedSubView.call(this, event, view);
+
+    }
+  });
+
+  var AddBadgesView = StoryBadgesEditorView.extend({
+    clickedSubView: function(event, view) {
+      this.storyBadges.addBadge(view.model);
+      StoryBadgesEditorView.prototype.clickedSubView.call(this, event, view);
+    }
+  });
+
+
   storybase.badges.views.BadgeView = BadgeView;
   storybase.badges.views.BadgesView = BadgesView;
+
+  storybase.badges.views.StoryBadgesEditorView = StoryBadgesEditorView;
+  storybase.badges.views.AddBadgesView = AddBadgesView;
+  storybase.badges.views.RemoveBadgesView = RemoveBadgesView;
 
 })(_, Backbone, storybase);
