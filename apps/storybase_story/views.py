d36788d551394ef0a315b19f0c9155cf26e1fb62
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-08-04 21:31:13 -0600

    Optimiztion of other homepage query

--- apps/storybase_story/views.py
+++ apps/storybase_story/views.py
@@ -39,7 +39,7 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['topics'] = Category.objects.all()
+        context['topics'] = Category.objects.all().values('categorytranslation__name', 'id')
         context['places'] = Place.objects.all().values('name', 'id')
 
         return context
