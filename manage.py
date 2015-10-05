8e6389a36dfa4ab84fd21f8144706fde0db2ffbf
Nolan Stover <nstover@fusionbox.com>
2015-10-05 14:18:18 -0600

    manage.py uses dotenv

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-import os
-import sys
+import os, sys
+import dotenv
+dotenv.read_dotenv()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
