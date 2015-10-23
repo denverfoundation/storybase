f68da1fd7e6edd6d68b364b0cc9643d61b47412e
Nolan Stover <nstover@fusionbox.com>
2015-10-23 13:47:12 -0600

    Remove username increase email length post sync action

--- apps/storybase_user/management/__init__.py
+++ apps/storybase_user/management/__init__.py
@@ -1,34 +1,8 @@
 from django.conf import settings
-from django.contrib.auth import models as auth_app
 from django.contrib.auth.models import User, Group
-from django.db.models.signals import post_save, post_syncdb
+from django.db.models.signals import post_syncdb
 
 import storybase_user as user_app
-
-# Based on django-email-usernames by Hakan Waara
-# https://bitbucket.org/hakanw/django-email-usernames
-def increase_username_and_email_length(sender, app, created_models, verbosity,
-                                       interactive, **kwargs):
-    """Signal handler to increase User field length on syncdb"""
-    from django.core.management import call_command
-
-    if (interactive and app.__name__ == 'django.contrib.auth.models' and
-            User not in created_models):
-        msg = ("\nYou have installed Django's auth system.  You can increase "
-               " the length of the username and email address fields to 254 "
-               " characters. If you don't do this, user registration may "
-               " not work properly for users with long email addresses "
-               "\nIncrease field length now? (yes/no): ")
-        confirm = raw_input(msg)
-        while 1:
-            if confirm not in ('yes', 'no'):
-                confirm = raw_input('Please enter either "yes" or "no": ')
-                continue
-            if confirm == 'yes':
-                call_command("incuserfieldlen", interactive=True)
-            break
-
-post_syncdb.connect(increase_username_and_email_length)
 
 
 def create_admin_group(sender, app, created_models, verbosity, interactive,
