9fbea0caaad6129da02e3a016d36299a0b9c47fc
Nolan Stover <nstover@fusionbox.com>
2015-10-27 15:35:15 -0600

    Rollover/Click on a story's badge to see what the badge stands for

--- static/js/badges/views.js
+++ static/js/badges/views.js
@@ -7,8 +7,14 @@
   var BadgeView = Backbone.View.extend({
     template: _.template($('#badge-template').html()),
     tagName: 'span',
+    events: {
+      'click .badge': 'clicked'
+    },
+    clicked: function(event) {
+      this.parent.clickedSubView(event, this);
+    },
     render: function() {
-      this.$el.html(this.template(this.model.attributes));
+      this.$el.html(this.template(this.model.attributes)).children('a').tooltipster();
       return this;
     }
   });
@@ -26,16 +32,16 @@
       }, this);
 
       return this;
-    }
-
-  });
-
-  var ClickableBadgeView = BadgeView.extend({
-    events: {
-      'click .badge': 'clicked'
     },
-    clicked: function(event) {
-      this.parent.clickedSubView(event, this);
+    clickedSubView: function(event, view) {
+        $('#show-badge').bPopup({
+            modalClose: true,
+            opacity: 0.6,
+            positionStyle: 'fixed'
+        })
+        .find('#show-badge-heading').html(view.model.get('name'))
+        .end()
+        .find('#show-badge-body').html(view.model.get('description'));
     }
   });
 
@@ -44,20 +50,18 @@
       this.userBadges = options.userBadges;
       this.storyBadges = options.storyBadges;
       this.badgesView = options.badgesView;
-
     },
     clickedSubView: function(event, view) {
       this.badgesView.render();
       view.remove();
     },
-    SubView: ClickableBadgeView,
+    SubView: BadgeView,
   });
 
   var RemoveBadgesView = StoryBadgesEditorView.extend({
     clickedSubView: function(event, view) {
       this.storyBadges.removeBadge(view.model);
       StoryBadgesEditorView.prototype.clickedSubView.call(this, event, view);
-
     }
   });
 
