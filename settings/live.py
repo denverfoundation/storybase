63b223c8d2ba984c33309e0828ebf003216f011c
Nolan Stover <nstover@fusionbox.com>
2016-01-22 14:03:01 -0700

    Use separate solr core config for live deployment

--- settings/live.py
+++ settings/live.py
@@ -18,6 +18,8 @@ DEFAULT_FROM_EMAIL = 'no-reply@floodlightproject.org'
 DATABASES = {'default': dj_database_url.config()}
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
+HAYSTACK_CONNECTIONS['default']['URL'] = 'http://127.0.0.1:8983/solr/floodlight_live'
+
 RAVEN_CONFIG = {
     'dsn': 'https://223a9ba8b85b4ba1b5ae2d09831e77e4:8d9c8e76d4854806b3cbf728e2276045@sentry.fusionbox.com/67'
 }
