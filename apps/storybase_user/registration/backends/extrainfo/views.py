5c82f0b68a7643d2ab0831150a23302f2111cb7f
Nolan Stover <nstover@fusionbox.com>
2016-03-24 15:43:14 -0600

    Fix bugs in registration views

--- apps/storybase_user/registration/backends/extrainfo/views.py
+++ apps/storybase_user/registration/backends/extrainfo/views.py
@@ -13,9 +13,9 @@ class ExtraInfoRegistrationView(RegistrationView):
     """Registration view that collects additional information"""
     form_class = ExtraInfoRegistrationForm
 
-    def register(self, request, form):
+    def register(self, form):
         data = form.cleaned_data
-        new_user = super(ExtraInfoRegistrationView, self).register(request, form)
+        new_user = super(ExtraInfoRegistrationView, self).register(form)
         new_user.username = data['username']
         new_user.first_name = data['first_name']
         new_user.last_name = data['last_name']
@@ -26,16 +26,20 @@ class ExtraInfoActivationView(ActivationView):
     def handle_activation_failure(self, activation_key):
         logger.warn("Activation with key \"%s\" failed" % activation_key)
 
-    def activate(self, request, activation_key):
-        activated_user = super(ExtraInfoActivationView, self).activate(request, activation_key)
+    def activate(self, *args, **kwargs):
+        activated_user = super(ExtraInfoActivationView, self).activate(*args, **kwargs)
+        import pdb
+        pdb.set_trace()
 
         if activated_user:
-            messages.success(request, _("Your account activation succeeded. Please log in below."))
+            messages.success(
+                self.request,
+                _("Your account activation succeeded. Please log in below.")
+            )
         else:
             self.handle_activation_failure(activation_key)
 
         return activated_user
 
-    def get_success_url(self, request, user):
+    def get_success_url(self, user):
         return ('auth_login', (), {})
-
