c760e0c89f0408bc66ab8f8e8c56a5504bc13694
Nolan Stover <nstover@fusionbox.com>
2016-03-31 10:10:34 -0600

    Update live email credentials

--- settings/live.py
+++ settings/live.py
@@ -7,10 +7,10 @@ STATIC_ROOT = os.environ['STATIC_ROOT']
 DEBUG = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = ''
-EMAIL_PORT = 465
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'floodlightproject@gmail.com'
+EMAIL_HOST_PASSWORD = 'A7BR$i9P!U&0zLRl'
 EMAIL_USE_TLS = True
 
 DEFAULT_FROM_EMAIL = 'no-reply@floodlightproject.org'
