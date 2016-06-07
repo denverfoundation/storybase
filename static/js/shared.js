e19e750c48e7f2e8d3a044e39f71adb4fd93a11e
Nolan Stover <nstover@fusionbox.com>
2016-06-07 13:48:28 -0600

    The "i" modifier in the attr selector is not yet supported enough
    It was causing a JS error to be thrown in Firefox.
    Instead, filter all <a> tags and test their lower-cased href attribute

--- static/js/shared.js
+++ static/js/shared.js
@@ -1,7 +1,10 @@
 (function($) {
   $(document).ready(function() {
     // External links
-    $('a[href^="http://" i], a[href^="https://" i]').attr('target', function(index, attr) {
+
+    $('a').filter(function() {
+        return /^https?:\/\//.test($(this).attr('href').toLowerCase());
+    }).attr('target', function(index, attr) {
       var href = ($(this).attr('href') || '').toLowerCase();
       if (href.indexOf(document.location.origin) === 0) {
         return attr;
