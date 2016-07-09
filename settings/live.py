bb0922c74a014abac474f5cfec4ea3cbccfe131f
Nolan Stover <nstover@fusionbox.com>
2016-07-08 18:15:21 -0600

    Revert "Change SMTP email info & contact email address"
    This reverts commit b77018ae8185e6c8a3e2c1b0741cac0befeb6032.

--- settings/live.py
+++ settings/live.py
@@ -10,7 +10,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'floodlightproject@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'A7BR$i9P!U&0zLRl'
 EMAIL_USE_TLS = True
 
 DEFAULT_FROM_EMAIL = 'no-reply@floodlightproject.org'
