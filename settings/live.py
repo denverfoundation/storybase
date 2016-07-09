1ec13c2c7a4bc8c6a0b16d1b9ebecd75b856ece7
Nolan Stover <nstover@fusionbox.com>
2016-07-08 18:00:40 -0600

    Use old smtp account until problems with new one are resolved

--- settings/live.py
+++ settings/live.py
@@ -9,12 +9,12 @@ DEBUG = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'floodlight@denverfoundation.org'
+EMAIL_HOST_USER = 'floodlightproject@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 DEFAULT_FROM_EMAIL = 'no-reply@floodlightproject.org'
-STORYBASE_CONTACT_EMAIL = "floodlight@denverfoundation.org"
+STORYBASE_CONTACT_EMAIL = 'floodlightproject@gmail.com'
 
 DATABASES = {'default': dj_database_url.config()}
 DATABASES['default']['ATOMIC_REQUESTS'] = True
